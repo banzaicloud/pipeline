766c51e536b3b68183f9cf0cf97bf06a726e45bc
Magyari Sandor Szilard <sancyx@gmail.com>
2018-01-09 18:45:47 +0100

    List pods per node on status endpoint (magyari_sandor)

--- pods/kubernetes.go
+++ pods/kubernetes.go
@@ -3,16 +3,19 @@ package pods
 import (
 	"flag"
 	"path/filepath"
-
-	utils "github.com/banzaicloud/pipeline/utils"
+	"github.com/banzaicloud/pipeline/utils"
 	log "github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	banzaiUtils "github.com/banzaicloud/banzai-types/utils"
 	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"github.com/banzaicloud/pipeline/cloud"
+	"github.com/kris-nova/kubicorn/apis/cluster"
+	"fmt"
 )
 
 //Get Kubernetes config - running inside or outside K8S
@@ -99,8 +102,79 @@ func groupPods(pods []corev1.Pod) (podGroup map[string][]corev1.Pod) {
 func getPodGroupName(pod *corev1.Pod) *string {
 	generatedName := pod.GenerateName
 	if len(generatedName) > 0 {
-		generatedName = generatedName[0 : len(generatedName)-1]
+		generatedName = generatedName[0: len(generatedName)-1]
 		return &generatedName
 	}
 	return nil
+}
+
+type PodStatusResponse struct {
+	Name   string          `json:"name"`
+	Status corev1.PodPhase `json:"status"`
+}
+
+func GroupPodsByNode(pods []corev1.Pod) (podGroup map[string][]PodStatusResponse) {
+	podGroup = make(map[string][]PodStatusResponse)
+	for _, pod := range pods {
+		groupName := &pod.Spec.NodeName
+
+		banzaiUtils.LogInfo(utils.TagStatus, "Pod name: %s status: %s running on node: %s", pod.Name, pod.Status.Phase, groupName)
+		podStatus := PodStatusResponse{
+			Name:   pod.Name,
+			Status: pod.Status.Phase,
+		}
+		podGroup[*groupName] = append(podGroup[*groupName], podStatus)
+
+	}
+	return podGroup
+}
+
+type ClusterStatusResponse struct {
+	Name       string                         `json:"name"`
+	SystemPods map[string][]PodStatusResponse `json:"systemPods"`
+	Pods       map[string][]PodStatusResponse `json:"userPods"`
+}
+
+func CreatePodMap(client *kubernetes.Clientset, namespace string) (map[string][]PodStatusResponse, error) {
+
+	podList, err := client.CoreV1().Pods(namespace).List(metav1.ListOptions{})
+	if err != nil && podList != nil {
+		return make(map[string][]PodStatusResponse), fmt.Errorf("PoD list failed: %v", err)
+	}
+
+	var podGroupMap map[string][]PodStatusResponse
+	podGroupMap = GroupPodsByNode(podList.Items)
+	return podGroupMap, nil
+}
+
+func ListPodsForCluster(cluster *cluster.Cluster) (ClusterStatusResponse, error) {
+	var (
+		config *rest.Config
+		err    error
+	)
+	kubeConfig, err := cloud.GetConfig(cluster, "")
+	if err != nil {
+		return ClusterStatusResponse{}, err
+	}
+	if kubeConfig != "" {
+		config, err = clientcmd.BuildConfigFromFlags("", kubeConfig)
+	}
+	if err != nil {
+		return ClusterStatusResponse{}, fmt.Errorf("K8S Connection Failed: %v", err)
+	}
+
+	client := kubernetes.NewForConfigOrDie(config)
+	systemPodGroupMap, err := CreatePodMap(client, metav1.NamespaceSystem)
+	if err != nil {
+		return ClusterStatusResponse{}, err
+	}
+	podGroupMap, err := CreatePodMap(client, metav1.NamespaceDefault)
+	if err != nil {
+		return ClusterStatusResponse{}, err
+	}
+	return ClusterStatusResponse{
+		Name:       cluster.Name,
+		SystemPods: systemPodGroupMap,
+		Pods:       podGroupMap,
+	}, nil
 }
