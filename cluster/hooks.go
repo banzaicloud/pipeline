18302ca6c1b1057a258b6ac4ecdc6ff2c89ce0e4
tarokkk <gubasanyi@gmail.com>
2018-02-28 09:46:49 +0100

    fixing log message

--- cluster/hooks.go
+++ cluster/hooks.go
@@ -32,7 +32,7 @@ func PersistKubernetesKeys(cluster CommonCluster) {
 	for i := 0; i < retryCount; i++ {
 		kubeConfig, err = cluster.GetK8sConfig()
 		if err != nil {
-			log.Infof(": %s", err)
+			log.Infof("Error getting kubernetes config attempt %s/%s: %s. Waiting %s seconds", i, retryCount, err.Error(), retrySleepTime)
 			time.Sleep(time.Duration(retrySleepTime) * time.Second)
 			continue
 		}
