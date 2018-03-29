017d09ae290fdfbb2f34740eb3bb0cad1e7d0e8a
Janos Matyas <janos.matyas@gmail.com>
2018-03-29 21:55:47 +0200

    Added required changes

--- cluster/dummy.go
+++ cluster/dummy.go
@@ -10,6 +10,7 @@ import (
 	"net/http"
 )
 
+// DummyCluster struct for DC
 type DummyCluster struct {
 	modelCluster *model.ClusterModel
 	k8sConfig    *[]byte
@@ -37,15 +38,18 @@ func CreateDummyClusterFromRequest(request *components.CreateClusterRequest, org
 	return &cluster, nil
 }
 
+//CreateCluster creates a new cluster
 func (d *DummyCluster) CreateCluster() error {
 	return nil
 }
 
+//Persist save the cluster model
 func (d *DummyCluster) Persist() error {
 	log.Infof("Model before save: %v", d.modelCluster)
 	return d.modelCluster.Save()
 }
 
+//GetK8sConfig returns the Kubernetes config
 func (d *DummyCluster) GetK8sConfig() (*[]byte, error) {
 	data, err := yaml.Marshal(createDummyConfig())
 	if err != nil {
@@ -55,14 +59,17 @@ func (d *DummyCluster) GetK8sConfig() (*[]byte, error) {
 	return &data, nil
 }
 
+//GetName returns the name of the cluster
 func (d *DummyCluster) GetName() string {
 	return d.modelCluster.Name
 }
 
+//GetType returns the cloud type of the cluster
 func (d *DummyCluster) GetType() string {
 	return constants.Dummy
 }
 
+//GetStatus gets cluster status
 func (d *DummyCluster) GetStatus() (*components.GetClusterStatusResponse, error) {
 	return &components.GetClusterStatusResponse{
 		Status:           http.StatusOK,
@@ -74,24 +81,29 @@ func (d *DummyCluster) GetStatus() (*components.GetClusterStatusResponse, error)
 	}, nil
 }
 
+// DeleteCluster deletes cluster 
 func (d *DummyCluster) DeleteCluster() error {
 	return nil
 }
 
+// UpdateCluster updates the dummy cluster 
 func (d *DummyCluster) UpdateCluster(r *components.UpdateClusterRequest) error {
 	d.modelCluster.Dummy.KubernetesVersion = r.UpdateClusterDummy.Node.KubernetesVersion
 	d.modelCluster.Dummy.NodeCount = r.UpdateClusterDummy.Node.Count
 	return nil
 }
 
+//GetID returns the specified cluster id
 func (d *DummyCluster) GetID() uint {
 	return d.modelCluster.ID
 }
 
+//GetModel returns the whole clusterModel
 func (d *DummyCluster) GetModel() *model.ClusterModel {
 	return d.modelCluster
 }
 
+//CheckEqualityToUpdate validates the update request
 func (d *DummyCluster) CheckEqualityToUpdate(r *components.UpdateClusterRequest) error {
 	return nil
 }
@@ -100,19 +112,23 @@ func (d *DummyCluster) AddDefaultsToUpdate(r *components.UpdateClusterRequest) {
 
 }
 
+//GetAPIEndpoint returns the Kubernetes Api endpoint
 func (d *DummyCluster) GetAPIEndpoint() (string, error) {
 	d.APIEndpoint = "http://cow.org:8080"
 	return d.APIEndpoint, nil
 }
 
+//DeleteFromDatabase deletes model from the database
 func (d *DummyCluster) DeleteFromDatabase() error {
 	return d.modelCluster.Delete()
 }
 
+//GetOrg gets org where the cluster belongs
 func (d *DummyCluster) GetOrg() uint {
 	return d.modelCluster.OrganizationId
 }
 
+//GetSecretID retrieves the secretid
 func (d *DummyCluster) GetSecretID() string {
 	return d.modelCluster.SecretId
 }
@@ -172,6 +188,7 @@ func createDummyConfig() *kubeConfig {
 
 }
 
+//CreateDummyClusterFromModel creates the cluster from the model
 func CreateDummyClusterFromModel(clusterModel *model.ClusterModel) (*DummyCluster, error) {
 	log := logger.WithFields(logrus.Fields{"action": constants.TagGetCluster})
 	log.Debug("Create ClusterModel struct from the request")
