6b3aa75460942ac5383a954e6364b3da4c719297
colin <kispiszkerf@gmail.com>
2018-05-25 13:40:16 +0200

    fix if block ends with a return statement...

--- cluster/hooks.go
+++ cluster/hooks.go
@@ -165,9 +165,8 @@ func InstallHelmPostHook(cluster CommonCluster) {
 			_, err = helm.GetHelmClient(kubeConfig)
 			if err == nil {
 				return
-			} else {
-				log.Debugf("Error during getting helm client: %s", err.Error())
 			}
+			log.Warnf("Error during getting helm client: %s", err.Error())
 			time.Sleep(time.Duration(retrySleepSeconds) * time.Second)
 		}
 		log.Error("Timeout during waiting for tiller to get ready")
