eab6047f4bc76d2947bc052a93d4591edd84f97d
tarokkk <gubasanyi@gmail.com>
2018-06-16 17:52:36 +0200

    revendor banzai-types

--- vendor/github.com/banzaicloud/banzai-types/components/catalog/catalog.go
+++ vendor/github.com/banzaicloud/banzai-types/components/catalog/catalog.go
@@ -2,53 +2,53 @@ package catalog
 
 // SpotguideFile to parse spotguide.yaml
 type SpotguideFile struct {
-	Resources *ApplicationResources   `json:"resources"`
-	Options   []ApplicationOptions    `json:"options"`
-	Depends   []ApplicationDependency `json:"depends"`
+	Resources *ApplicationResources   `json:"resources" yaml:"resources"`
+	Options   []ApplicationOptions    `json:"options" yaml:"options"`
+	Depends   []ApplicationDependency `json:"depends" yaml:"depends"`
 }
 
 // ApplicationOptions for API response
 type ApplicationOptions struct {
-	Name     string   `json:"name"`
-	Type     string   `json:"type"`
-	Default  bool     `json:"default"`
-	Label    string   `json:"label"`
-	Info     string   `json:"info"`
-	Readonly bool     `json:"readonly"`
-	Key      string   `json:"key"`
-	Value    string   `json:"value"`
-	Enum     []string `json:"enum"`
+	Name     string   `json:"name" yaml:"name"`
+	Type     string   `json:"type" yaml:"type"`
+	Default  string   `json:"default" yaml:"default"`
+	Label    string   `json:"label" yaml:"label"`
+	Info     string   `json:"info" yaml:"info"`
+	Readonly bool     `json:"readonly" yaml:"readonly"`
+	Key      string   `json:"key" yaml:"key"`
+	Value    string   `json:"value" yaml:"value"`
+	Enum     []string `json:"enum" yaml:"enum"`
 }
 
 // ApplicationDependency for spotguide.yaml
 type ApplicationDependency struct {
-	Info      string           `json:"info"`
-	Name      string           `json:"name"`
-	Type      string           `json:"type"`
-	Values    []string         `json:"values"`
-	Namespace string           `json:"namespace"`
-	Chart     ApplicationChart `json:"chart"`
-	Timeout   int              `json:"timeout"`
-	Retry     int              `json:"retry"`
+	Info      string           `json:"info" yaml:"info"`
+	Name      string           `json:"name" yaml:"name"`
+	Type      string           `json:"type" yaml:"type"`
+	Values    []string         `json:"values" yaml:"values"`
+	Namespace string           `json:"namespace" yaml:"namespace"`
+	Chart     ApplicationChart `json:"chart" yaml:"chart"`
+	Timeout   int              `json:"timeout" yaml:"timeout"`
+	Retry     int              `json:"retry" yaml:"retry"`
 }
 
 // ApplicationChart for spotguide.yaml
 type ApplicationChart struct {
-	Name       string `json:"name"`
-	Repository string `json:"repository"`
-	Version    string `json:"version"`
+	Name       string `json:"name" yaml:"name"`
+	Repository string `json:"repository" yaml:"repository"`
+	Version    string `json:"version" yaml:"version"`
 }
 
 // ApplicationResources to parse spotguide.yaml
 type ApplicationResources struct {
-	SumCpu      float64  `json:"sumCpu" binding:"min=1"`
-	SumMem      float64  `json:"sumMem" binding:"min=1"`
-	MinNodes    int      `json:"minNodes,omitempty" binding:"min=1,ltefield=MaxNodes"`
-	MaxNodes    int      `json:"maxNodes,omitempty"`
-	SameSize    bool     `json:"sameSize,omitempty"`
-	OnDemandPct int      `json:"onDemandPct,omitempty" binding:"min=1,max=100"`
-	Zones       []string `json:"zones,omitempty" binding:"dive,zone"`
-	SumGpu      int      `json:"sumGpu,omitempty"`
-	AllowBurst  *bool    `json:"allowBurst,omitempty"`
-	NetworkPerf *string  `json:"networkPerf,omitempty"`
+	SumCpu      float64  `json:"sumCpu" yaml:"sumCpu"`
+	SumMem      float64  `json:"sumMem" yaml:"sumMem"`
+	MinNodes    int      `json:"minNodes,omitempty" yaml:"minNodes,omitempty"`
+	MaxNodes    int      `json:"maxNodes,omitempty" yaml:"maxNodes,omitempty"`
+	SameSize    bool     `json:"sameSize,omitempty" yaml:"sameSize,omitempty"`
+	OnDemandPct int      `json:"onDemandPct,omitempty" yaml:"onDemandPct,omitempty" binding:"min=1,max=100"`
+	Zones       []string `json:"zones,omitempty" yaml:"zones,omitempty" binding:"dive,zone"`
+	SumGpu      int      `json:"sumGpu,omitempty" yaml:"sumGpu,omitempty"`
+	AllowBurst  *bool    `json:"allowBurst,omitempty" yaml:"allowBurst,omitempty"`
+	NetworkPerf *string  `json:"networkPerf,omitempty" yaml:"networkPerf,omitempty"`
 }
