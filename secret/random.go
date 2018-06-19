40fa7924e418d08cdaf46cf9696bb605106a4a98
Ferenc HERNADI <ferenc.hernadi@gmail.com>
2018-06-19 23:20:36 +0200

    random generator test

--- secret/random.go
+++ secret/random.go
@@ -5,34 +5,20 @@ import (
 	"github.com/aokoli/goutils"
 )
 
-func Generator(genType string, length int) (string, error) {
+//RandomString creates a random string whose length is the number of characters specified.
+func RandomString(genType string, length int) (res string, err error) {
 	switch genType {
 	case "randAlphaNum":
-		res, err := goutils.RandomAlphaNumeric(length)
-		if err != nil {
-			return "", err
-		}
-		return res, nil
+		res, err = goutils.RandomAlphaNumeric(length)
 	case "randAlpha":
-		res, err := goutils.RandomAlphabetic(length)
-		if err != nil {
-			return "", err
-		}
-		return res, nil
+		res, err = goutils.RandomAlphabetic(length)
 	case "randNumeric":
-		res, err := goutils.RandomNumeric(length)
-		if err != nil {
-			return "", err
-		}
-		return res, nil
+		res, err = goutils.RandomNumeric(length)
 	case "randAscii":
-		res, err := goutils.RandomAscii(length)
-		if err != nil {
-			return "", err
-		}
-		return res, nil
+		res, err = goutils.RandomAscii(length)
 	default:
-		return "", fmt.Errorf("unsupported random type: %s", genType)
+		return res, fmt.Errorf("unsupported random type: %s", genType)
 	}
+	return
 
 }
