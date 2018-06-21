1817e12d9aff353bebb571e496e8ccc4a1b13a56
tarokkk <gubasanyi@gmail.com>
2018-06-21 11:23:47 +0200

    create common

---
+++ pkg/common/common.go
@@ -0,0 +1,14 @@
+package common
+
+// BanzaiResponse describes Pipeline's responses
+type BanzaiResponse struct {
+	StatusCode int    `json:"status_code,omitempty"`
+	Message    string `json:"message,omitempty"`
+}
+
+// ErrorResponse describes Pipeline's responses when an error occurred
+type ErrorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Error   string `json:"error"`
+}
