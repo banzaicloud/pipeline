743fffecc294796eac42eaf6ddc939b4b08d43d1
Ferenc HERNADI <ferenc.hernadi@gmail.com>
2018-06-30 22:43:12 +0200

    New amazon provider kubernetes bootstrap script url.

--- cluster/aws.go
+++ cluster/aws.go
@@ -894,8 +894,8 @@ func DownloadK8sConfig(kubicornCluster *kcluster.Cluster, organizationID uint, k
 const (
 	BootstrapScriptMasterKey     = "BOOTSTRAP_SCRIPT_MASTER"
 	BootstrapScriptNodeKey       = "BOOTSTRAP_SCRIPT_NODE"
-	BootstrapScriptMasterDefault = "https://raw.githubusercontent.com/banzaicloud/banzai-charts/master/stable/pipeline/bootstrap/amazon_k8s_ubuntu_16.04_master_pipeline.sh"
-	BootstrapScriptNodeDefault   = "https://raw.githubusercontent.com/banzaicloud/banzai-charts/master/stable/pipeline/bootstrap/amazon_k8s_ubuntu_16.04_node_pipeline.sh"
+	BootstrapScriptMasterDefault = "https://raw.githubusercontent.com/banzaicloud/banzai-charts/master/pipeline/bootstrap/amazon_k8s_ubuntu_16.04_master_pipeline.sh"
+	BootstrapScriptNodeDefault   = "https://raw.githubusercontent.com/banzaicloud/banzai-charts/master/pipeline/bootstrap/amazon_k8s_ubuntu_16.04_node_pipeline.sh"
 )
 
 func getBootstrapScriptFromEnv(isMaster bool) string {
