496d09e90da76703355721d743cce60ba9622b4d
colin <kispiszkerf@gmail.com>
2018-08-10 08:55:43 +0200

    rename oracle json key to oke

--- pkg/providers/oracle/model/profile.go
+++ pkg/providers/oracle/model/profile.go
@@ -137,7 +137,7 @@ func (d *Profile) GetProfile() *pkgCluster.ClusterProfileResponse {
 			EKS  *eks.ClusterProfileEKS   `json:"eks,omitempty"`
 			AKS  *aks.ClusterProfileAKS   `json:"aks,omitempty"`
 			GKE  *gke.ClusterProfileGKE   `json:"gke,omitempty"`
-			OKE  *oracle.Cluster          `json:"oracle,omitempty"`
+			OKE  *oracle.Cluster          `json:"oke,omitempty"`
 		}{
 			OKE: &oracle.Cluster{
 				Version:   d.Version,
