f8e5a4bbf3faad679ee1dd9bb483071fe19b9ad9
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2018-09-03 12:58:51 +0200

    Remove model dependency from monitoring

--- cluster/manager_get.go
+++ cluster/manager_get.go
@@ -40,6 +40,36 @@ func (m *Manager) GetClusters(ctx context.Context, organizationID uint) ([]Commo
 	return clusters, nil
 }
 
+// GetAllCLusters returns all cluster instances.
+func (m *Manager) GetAllClusters(ctx context.Context) ([]CommonCluster, error) {
+	logger := m.getLogger(ctx)
+
+	logger.Debug("fetching clusters from database")
+
+	clusterModels, err := m.clusters.All()
+	if err != nil {
+		return nil, err
+	}
+
+	var clusters []CommonCluster
+
+	for _, clusterModel := range clusterModels {
+		logger := logger.WithField("cluster", clusterModel.Name)
+		logger.Debug("converting cluster model to common cluster")
+
+		cluster, err := GetCommonClusterFromModel(clusterModel)
+		if err != nil {
+			logger.Error("converting cluster model to common cluster failed")
+
+			continue
+		}
+
+		clusters = append(clusters, cluster)
+	}
+
+	return clusters, nil
+}
+
 // GetClusterByID returns the cluster instance for an organization ID by cluster ID.
 func (m *Manager) GetClusterByID(ctx context.Context, organizationID uint, clusterID uint) (CommonCluster, error) {
 	logger := m.getLogger(ctx).WithFields(logrus.Fields{
