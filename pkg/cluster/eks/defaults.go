c4b9a92f3625ce61bfccc47a6dfb8a0d020d83e5
Magyari Sandor Szilard <sancyx@gmail.com>
2018-09-06 11:57:44 +0200

    EKS: update AMI's & nodegroup cf template

--- pkg/cluster/eks/defaults.go
+++ pkg/cluster/eks/defaults.go
@@ -7,8 +7,8 @@ const (
 
 // DefaultImages in each supported location in EC2 (from https://docs.aws.amazon.com/eks/latest/userguide/launch-workers.html)
 var DefaultImages = map[string]string{
-	UsEast1: "ami-0fef2bff3c2e2da93",
-	UsWest2: "ami-0ea01e1d1dea65b5c",
+	UsEast1: "ami-0b2ae3c6bda8b5c06",
+	UsWest2: "ami-08cab282f9979fc7a",
 }
 
 // EC2 regions
