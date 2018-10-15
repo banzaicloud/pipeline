4c1a590b8a44b2e172d444c27242f84519baf117
Ferenc HERNADI <ferenc.hernadi@gmail.com>
2018-10-15 14:34:43 +0200

    New line fix + rebase

--- scripts/debug_builder_proxy.sh
+++ scripts/debug_builder_proxy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash -e
-#cd $1
+
 OLD_POD=$(kubectl get po -l app=controlplane-pipeline --sort-by=.metadata.creationTimestamp -o=name|tail -n1 )
 echo Old POD name: $OLD_POD
+
 time make debug-docker
 echo "DELETE the old POD: $OLD_POD"
 kubectl delete $OLD_POD
+
 sleep 5
+
 echo "Force DELETE the old POD: $OLD_POD"
 kubectl delete $OLD_POD --grace-period=0 --force
+
 NEW_POD=$(kubectl get po -l app=controlplane-pipeline --sort-by=.metadata.creationTimestamp -o=name|tail -n1 )
 NEW_POD_STATUS=""
 echo "New POD Name: $NEW_POD"
@@ -18,5 +22,6 @@ while [[ "$NEW_POD_STATUS" != "Running" ]] ; do
     echo "New POD Status: $NEW_POD_STATUS"
 done
 osascript -e 'display notification "Proxy activated" with title "New Build Deployed"'
+
 echo "Start Proxy"
 kubectl port-forward $NEW_POD 40000
