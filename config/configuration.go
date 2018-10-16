620b0d924990477c14604825503b2f1ec72d2c29
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2018-10-16 14:30:04 +0200

    Disable automatic migrations by default

--- config/configuration.go
+++ config/configuration.go
@@ -88,6 +88,9 @@ const (
 	ARKBucketSyncInterval  = "ark.bucketSyncInterval"
 	ARKRestoreSyncInterval = "ark.restoreSyncInterval"
 	ARKBackupSyncInterval  = "ark.backupSyncInterval"
+
+	// Database
+	DBAutoMigrateEnabled = "database.autoMigrateEnabled"
 )
 
 //Init initializes the configurations
@@ -141,6 +144,7 @@ func init() {
 	viper.SetDefault("database.password", "pipemaster123!")
 	viper.SetDefault("database.dbname", "pipelinedb")
 	viper.SetDefault("database.logging", false)
+	viper.SetDefault(DBAutoMigrateEnabled, false)
 	viper.SetDefault("audit.enabled", true)
 	viper.SetDefault("audit.headers", []string{"secretId"})
 	viper.SetDefault("audit.skippaths", []string{"/auth/github/callback", "/pipeline/api"})
