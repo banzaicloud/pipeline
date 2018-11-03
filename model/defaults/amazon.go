162ad2ae06acbe116b0d4c2bd762f602fdc84081
Toader, Sebastian <st0ad3r@gmail.com>
2018-11-03 08:52:01 +0100

    Add migration scripts

--- model/defaults/amazon.go
+++ model/defaults/amazon.go
@@ -19,7 +19,7 @@ type AmazonNodePoolProfileBaseFields struct {
 	InstanceType string `gorm:"default:'m4.xlarge'"`
 	Name         string `gorm:"unique_index:idx_name_node_name"`
 	NodeName     string `gorm:"unique_index:idx_name_node_name"`
-	SpotPrice    string `gorm:"default:'0.0'"`
+	SpotPrice    string
 	Autoscaling  bool   `gorm:"default:false"`
 	MinCount     int    `gorm:"default:1"`
 	MaxCount     int    `gorm:"default:2"`
