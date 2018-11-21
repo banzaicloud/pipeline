09028bc3c1763e7150200a7f206e6e03d05e254a
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2018-11-21 10:31:18 +0100

    Use bank-vaults cert generation implementation

--- pkg/crypto/cert/generator.go
+++ pkg/crypto/cert/generator.go
@@ -15,69 +15,37 @@
 package cert
 
 import (
-	"bytes"
 	"crypto"
 	"crypto/ecdsa"
-	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
-	"io"
 	"io/ioutil"
-	"math/big"
 	"time"
 
-	"github.com/goph/emperror"
+	"github.com/banzaicloud/bank-vaults/pkg/tls"
 	"github.com/pkg/errors"
 )
 
 const (
-	privateKeyBits = 2048
-	validity       = 365 * 24 * time.Hour
+	validity = 365 * 24 * time.Hour
 )
-
-type Clock interface {
-	Now() time.Time
-}
-
-// SystemClock uses the real time.
-// TODO: install tardis package
-var SystemClock = NewSystemClock()
-
-type systemClock struct{}
-
-// NewSystemClock returns a clock that uses real time.
-func NewSystemClock() Clock {
-	return &systemClock{}
-}
-
-// Now tells the current time.
-func (*systemClock) Now() time.Time {
-	return time.Now()
-}
 
 // Generator generates a self-signed cert-key pair.
 type Generator struct {
 	signerCert *x509.Certificate
 	signerKey  crypto.Signer
-
-	clock      Clock
-	randReader io.Reader
 }
 
 // NewGenerator returns a new Generator instance.
 func NewGenerator(
 	signerCert *x509.Certificate,
 	signerKey crypto.Signer,
-	clock Clock,
-	randReader io.Reader,
 ) *Generator {
 	return &Generator{
 		signerCert: signerCert,
 		signerKey:  signerKey,
-		clock:      clock,
-		randReader: randReader,
 	}
 }
 
@@ -85,8 +53,6 @@ func NewGenerator(
 func NewGeneratorFromFile(
 	signerCertPath string,
 	signerKeyPath string,
-	clock Clock,
-	randReader io.Reader,
 ) (*Generator, error) {
 	signerCertFile, err := ioutil.ReadFile(signerCertPath)
 	if err != nil {
@@ -118,7 +84,7 @@ func NewGeneratorFromFile(
 		return nil, errors.Wrap(err, "failed to parse CA key")
 	}
 
-	return NewGenerator(signerCert, signerKey, clock, randReader), nil
+	return NewGenerator(signerCert, signerKey), nil
 }
 
 func parsePrivateKey(der []byte) (crypto.Signer, error) {
@@ -151,68 +117,17 @@ type CertificateRequest struct {
 
 // Generate generates a self-signed cert-key pair.
 func (g *Generator) Generate(request CertificateRequest) ([]byte, []byte, error) {
-	notBefore := g.clock.Now()
-	notAfter := notBefore.Add(validity)
-
-	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(g.randReader, serialNumberLimit)
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to generate a serial number for the certificate")
-	}
-
-	privateKey, err := rsa.GenerateKey(rand.Reader, privateKeyBits)
-	if err != nil {
-		return nil, nil, errors.Wrap(err, "failed to generate private key")
-	}
-
-	privateKeyBytes, err := keyToBytes(privateKey)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	certTemplate := &x509.Certificate{
-		SerialNumber: serialNumber,
+	certRequest := tls.ServerCertificateRequest{
 		Subject: pkix.Name{
 			CommonName: request.CommonName,
 		},
-		DNSNames:              request.AlternativeNames,
-		NotBefore:             notBefore,
-		NotAfter:              notAfter,
-		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		BasicConstraintsValid: true,
+		DNSNames: request.AlternativeNames,
+		Validity: validity,
 	}
-
-	cert, err := x509.CreateCertificate(g.randReader, certTemplate, g.signerCert, privateKey.Public(), g.signerKey)
+	cert, err := tls.GenerateServerCertificate(certRequest, g.signerCert, g.signerKey)
 	if err != nil {
-		return nil, nil, emperror.Wrap(err, "failed to sign certificate")
+		return nil, nil, errors.WithMessage(err, "failed to generate server certificate")
 	}
 
-	certBytes, err := certToBytes(cert)
-	if err != nil {
-		return nil, nil, err
-	}
-
-	return certBytes, privateKeyBytes, nil
-}
-
-func keyToBytes(key *rsa.PrivateKey) ([]byte, error) {
-	keyBytes := x509.MarshalPKCS1PrivateKey(key)
-	buffer := bytes.NewBuffer(nil)
-
-	if err := pem.Encode(buffer, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: keyBytes}); err != nil {
-		return nil, errors.Wrap(err, "failed to pem-encode key")
-	}
-
-	return buffer.Bytes(), nil
-}
-
-func certToBytes(certBytes []byte) ([]byte, error) {
-	buffer := bytes.NewBuffer(nil)
-
-	if err := pem.Encode(buffer, &pem.Block{Type: "CERTIFICATE", Bytes: certBytes}); err != nil {
-		return nil, errors.Wrap(err, "failed to pem-encode cert")
-	}
-
-	return buffer.Bytes(), nil
+	return cert.Certificate, cert.Key, nil
 }
