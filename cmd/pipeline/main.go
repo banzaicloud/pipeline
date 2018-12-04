65baeb3ebd47a02efa7b3af681e17e6f7ceae291
András Tóth <andras.toth@banzaicloud.com>
2018-12-04 22:44:04 +0100

    Revert "Revert "feat: sync per organization & shared spotguides (#1485)""
    This reverts commit 9491bb5f645ec41fe0e577db82480d54367d5db6.

--- cmd/pipeline/main.go
+++ cmd/pipeline/main.go
@@ -22,6 +22,8 @@ import (
 	"strings"
 	"time"
 
+	"github.com/banzaicloud/pipeline/spotguide"
+
 	evbus "github.com/asaskevich/EventBus"
 	"github.com/banzaicloud/go-gin-prometheus"
 	"github.com/banzaicloud/pipeline/api"
@@ -101,6 +103,20 @@ func main() {
 	if err != nil {
 		logger.Panic(err.Error())
 	}
+
+	// periodically sync shared spotguides
+	syncTicker := time.NewTicker(viper.GetDuration(config.SpotguideSyncInterval))
+	go func() {
+		if err := spotguide.ScrapeSharedSpotguides(); err != nil {
+			logger.Errorln("failed to sync shared spotguides", err)
+		}
+
+		for range syncTicker.C {
+			if err := spotguide.ScrapeSharedSpotguides(); err != nil {
+				logger.Errorln("failed to sync shared spotguides", err)
+			}
+		}
+	}()
 
 	basePath := viper.GetString("pipeline.basepath")
 
