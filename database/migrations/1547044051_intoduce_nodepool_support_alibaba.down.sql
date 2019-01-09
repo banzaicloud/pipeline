9e2bd554faf4effd596190fe9d37751e5a56d785
Balint Molnar <balintmolnar91@gmail.com>
2019-01-09 15:39:09 +0100

    Add migration scripts to alibaba nodepools

---
+++ database/migrations/1547044051_intoduce_nodepool_support_alibaba.down.sql
@@ -0,0 +1,8 @@
+ALTER TABLE `alibaba_acsk_node_pools` ADD COLUMN `count` int(11) DEFAULT NULL;
+ALTER TABLE `alibaba_acsk_node_pools` ADD COLUMN `image` varchar(255) DEFAULT NULL;
+ALTER TABLE `alibaba_acsk_node_pools` ADD COLUMN `system_disk_category` varchar(255) DEFAULT NULL;
+ALTER TABLE `alibaba_acsk_node_pools` ADD COLUMN `system_disk_size` int(11) DEFAULT NULL;
+ALTER TABLE `alibaba_acsk_node_pools` DROP COLUMN `min_count`;
+ALTER TABLE `alibaba_acsk_node_pools` DROP COLUMN `max_count`;
+ALTER TABLE `alibaba_acsk_node_pools` DROP COLUMN `asg_id`;
+ALTER TABLE `alibaba_acsk_node_pools` DROP COLUMN `scaling_conf_id`;
