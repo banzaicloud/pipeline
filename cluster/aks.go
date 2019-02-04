e26079b5eaa9c62a949c310b47d108758d0239ab
László Bence Nagy <laszlonagy93@gmail.com>
2019-02-04 14:36:32 +0100

    Implement GetK8sIpv4Cidrs() on AKS

--- cluster/aks.go
+++ cluster/aks.go
@@ -933,9 +933,18 @@ func (c *AKSCluster) SaveSshSecretId(sshSecretID string) error {
 	return c.modelCluster.UpdateSshSecret(sshSecretID)
 }
 
+// GetK8sIpv4Cidrs returns possible IP ranges for pods and services in the cluster
+// On AKS the services and pods IP ranges can be fetched from Azure
 func (c *AKSCluster) GetK8sIpv4Cidrs() (*pkgCluster.Ipv4Cidrs, error) {
-	//TODO
-	return nil, errors.New("not implemented")
+	cluster, err := c.getAzureCluster()
+	if err != nil {
+		return nil, emperror.Wrap(err, "failed to retrieve AKS cluster")
+	}
+
+	return &pkgCluster.Ipv4Cidrs{
+		ServiceClusterIPRanges: []string{*cluster.NetworkProfile.ServiceCidr},
+		PodIPRanges:            []string{*cluster.NetworkProfile.PodCidr},
+	}, nil
 }
 
 // GetK8sConfig returns the Kubernetes config
