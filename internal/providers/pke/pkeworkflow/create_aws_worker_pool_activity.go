3996bbde647c753245658bac0b149e350766fb8f
Tamás Ecsédi <ecsy.argh@gmail.com>
2019-02-21 16:05:26 +0100

    Fix spot price parameter with float parsing

--- internal/providers/pke/pkeworkflow/create_aws_worker_pool_activity.go
+++ internal/providers/pke/pkeworkflow/create_aws_worker_pool_activity.go
@@ -94,7 +94,8 @@ func (a *CreateWorkerPoolActivity) Execute(ctx context.Context, input CreateWork
 		return "", emperror.Wrap(err, "loading CF template")
 	}
 
-	if input.Pool.SpotPrice == "0.0" {
+	spotPrice, err := strconv.ParseFloat(input.Pool.SpotPrice, 64)
+	if err != nil || spotPrice <= 0.0 {
 		input.Pool.SpotPrice = ""
 	}
 
