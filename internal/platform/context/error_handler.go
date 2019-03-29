8ff6815e0ca4c51cd89ea87c486ffa9d14077231
Mate Ory <mate.ory@banzaicloud.com>
2019-03-29 14:51:46 +0100

    cluster: fix logging style

--- internal/platform/context/error_handler.go
+++ internal/platform/context/error_handler.go
@@ -20,7 +20,7 @@ import (
 	"github.com/goph/emperror"
 )
 
-// ErrorHandlerWithCorrelationID returns a new error handler with a correlation ID in it's context.
+// ErrorHandlerWithCorrelationID returns a new error handler with a correlation ID in its context.
 func ErrorHandlerWithCorrelationID(ctx context.Context, errorHandler emperror.Handler) emperror.Handler {
 	cid, ok := ctx.Value(contextKeyCorrelationId).(string)
 	if !ok || cid == "" {
