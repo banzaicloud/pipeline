d33a89193658eb0e9ebd4f2c2101c099f37f8632
Dudás Ádám <adam.dudas@banzaicloud.com>
2019-04-02 13:31:55 +0200

    add GORM implementation for PKE-on-Azure cluster store

---
+++ internal/providers/azure/pke/adapter/gorm_store_test.go
@@ -0,0 +1,44 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package adapter
+
+import (
+	"testing"
+
+	"github.com/banzaicloud/pipeline/internal/cluster"
+	"github.com/banzaicloud/pipeline/internal/providers/azure/pke"
+	"gotest.tools/assert"
+)
+
+func TestFillClusterFromClusterModel(t *testing.T) {
+	cases := []struct {
+		name     string
+		input    cluster.ClusterModel
+		expected pke.PKEOnAzureCluster
+	}{
+		{
+			name:     "empty cluster model",
+			input:    cluster.ClusterModel{},
+			expected: pke.PKEOnAzureCluster{},
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			var result pke.PKEOnAzureCluster
+			fillClusterFromClusterModel(&result, tc.input)
+			assert.DeepEqual(t, tc.expected, result)
+		})
+	}
+}
