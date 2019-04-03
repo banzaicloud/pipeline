dd3fea83b8a1410768e93e3bbcdcc99e26879a87
Balint Molnar <balintmolnar91@gmail.com>
2019-04-03 17:15:27 +0200

    Remove break which prevented to collect all tagged deployments

--- helm/helm.go
+++ helm/helm.go
@@ -278,7 +278,6 @@ func ListDeployments(filter *string, tagFilter string, kubeConfig []byte) (*rls.
 			if DeploymentHasTag(deployment.Deployment, tagFilter) {
 				filteredResp.Releases = append(filteredResp.Releases, deployment.Release)
 				filteredResp.Count++
-				break
 			}
 		}
 
