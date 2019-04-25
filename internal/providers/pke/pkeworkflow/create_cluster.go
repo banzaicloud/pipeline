2f15dc919af85eeff5a6b72248474c3087aab42b
Tamás Ecsédi <ecsy.argh@gmail.com>
2019-04-25 10:59:49 +0200

    Bump PKE version

--- internal/providers/pke/pkeworkflow/create_cluster.go
+++ internal/providers/pke/pkeworkflow/create_cluster.go
@@ -24,7 +24,7 @@ import (
 )
 
 const CreateClusterWorkflowName = "pke-create-cluster"
-const pkeVersion = "0.3.0-dev.5"
+const pkeVersion = "0.4.0"
 
 func getDefaultImageID(region, kubernetesVersion string) string {
 	switch kubernetesVersion {
