fcb71b04ad4884f7632c224627f71bea356a24bb
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-06-17 19:07:02 +0200

    Upgrade cadence

--- database/docker-init-mysql.sql
+++ database/docker-init-mysql.sql
@@ -1,2 +1,8 @@
 CREATE DATABASE IF NOT EXISTS cicd;
 GRANT ALL PRIVILEGES ON cicd.* TO 'sparky'@'%';
+
+CREATE DATABASE IF NOT EXISTS cadence;
+GRANT ALL PRIVILEGES ON cadence.* TO 'sparky'@'%';
+
+CREATE DATABASE IF NOT EXISTS cadence_visibility;
+GRANT ALL PRIVILEGES ON cadence_visibility.* TO 'sparky'@'%';
