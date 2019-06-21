ec5e11a0b30979beab3b303f869af263cb845427
Laszlo Puskas <laszlo@banzaicloud.com>
2019-06-21 14:34:49 +0200

    draft featureselector

---
+++ internal/clusterfeature/features.go
@@ -0,0 +1,60 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clusterfeature
+
+import (
+	"context"
+	"errors"
+
+	"github.com/goph/logur"
+)
+
+const (
+	externalDns = "external-dns"
+)
+
+type ExternalDnsFeature struct {
+	// chart details : name, version?
+	DomainFilters []string
+	Provider      string
+	Credentials   interface{}
+}
+
+// FeatureSelector operations for identifying supported features.
+type FeatureSelector interface {
+	// SelectFeature selects the feature to be worked with, eventually decorates it with internal information
+	SelectFeature(ctx context.Context, feature Feature) (*Feature, error)
+}
+
+type featureSelector struct {
+	logger logur.Logger
+}
+
+func (fs *featureSelector) SelectFeature(ctx context.Context, feature Feature) (*Feature, error) {
+	switch feature.Name {
+	case externalDns:
+		feature.Spec["chartName"] = "externaldns"
+		feature.Spec["chartVersion"] = "version"
+		return &feature, nil
+	}
+
+	return nil, errors.New("unsupported feature")
+}
+
+func NewFeatureSelector(logger logur.Logger) FeatureSelector {
+	return &featureSelector{
+		logger: logger,
+	}
+}
---
+++ internal/clusterfeature/features_test.go
@@ -0,0 +1,65 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package clusterfeature
+
+import (
+	"context"
+	"testing"
+
+	"github.com/goph/logur"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFeatureSelector_SelectFeature(t *testing.T) {
+	tests := []struct {
+		name    string
+		feature Feature
+		checker func(t *testing.T, fp *Feature, err error)
+	}{
+		{
+			name: "unsupported feature",
+			feature: Feature{
+				Name: "unsupported",
+				Spec: map[string]interface{}{},
+			},
+			checker: func(t *testing.T, fp *Feature, err error) {
+				assert.NotNil(t, err)
+				assert.Nil(t, fp)
+			},
+		},
+		{
+			name: "supported feature",
+			feature: Feature{
+				Name: externalDns,
+				Spec: map[string]interface{}{},
+			},
+			checker: func(t *testing.T, fp *Feature, err error) {
+				assert.Nil(t, err)
+				assert.NotNil(t, fp)
+				assert.Equal(t, "externaldns", fp.Spec["chartName"])
+			},
+		},
+	}
+
+	fs := NewFeatureSelector(logur.NewTestLogger())
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			fp, e := fs.SelectFeature(context.Background(), test.feature)
+			test.checker(t, fp, e)
+		})
+	}
+
+}
