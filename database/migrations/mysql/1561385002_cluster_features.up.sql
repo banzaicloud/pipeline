89a0b4a6e85304b0eba32618033d5dc350220847
Laszlo Puskas <laszlo@banzaicloud.com>
2019-06-24 16:49:42 +0200

    fix db creation script- mysql

--- database/migrations/mysql/1561385002_cluster_features.up.sql
+++ database/migrations/mysql/1561385002_cluster_features.up.sql
@@ -1,14 +1,17 @@
-CREATE TABLE IF NOT EXISTS "clusterfeature"
+CREATE TABLE `clusterfeature`
 (
-    "id"         serial,
-    "name"       text,
-    "cluster_id" integer,
-    "spec"       text,
-    "status"     text,
-    "created_at" timestamp with time zone,
-    "updated_at" timestamp with time zone,
-    "deleted_at" timestamp with time zone,
-    "created_by" integer,
-    PRIMARY KEY ("id")
-);
+    `id`         int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `name`       varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `cluster_id` int(10) unsigned                        DEFAULT NULL,
+    `spec`       text,
+    `status`     text,
+    `created_at` timestamp        NULL                   DEFAULT NULL,
+    `updated_at` timestamp        NULL                   DEFAULT NULL,
+    `deleted_at` timestamp        NULL                   DEFAULT NULL,
+    `created_by` int(10) unsigned                        DEFAULT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci;
+
 
