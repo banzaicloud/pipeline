778ddb1c7830091003d5b3fb265175553737f431
Dudás Ádám <adam.dudas@banzaicloud.com>
2019-07-04 13:17:27 +0200

    register cluster feature workflow and activities

---
+++ cmd/worker/clusterfeature.go
@@ -0,0 +1,52 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"go.uber.org/cadence/activity"
+	"go.uber.org/cadence/workflow"
+
+	"github.com/banzaicloud/pipeline/internal/clusterfeature"
+	clusterfeatureworkflow "github.com/banzaicloud/pipeline/internal/clusterfeature/clusterfeatureadapter/workflow"
+)
+
+func registerClusterFeatureWorkflows(featureRegistry clusterfeature.FeatureRegistry, featureRepository clusterfeature.FeatureRepository) {
+	workflow.RegisterWithOptions(clusterfeatureworkflow.ClusterFeatureJobWorkflow, workflow.RegisterOptions{Name: clusterfeatureworkflow.ClusterFeatureJobWorkflowName})
+
+	{
+		a := clusterfeatureworkflow.MakeClusterFeatureActivateActivity(featureRegistry)
+		activity.RegisterWithOptions(a.Execute, activity.RegisterOptions{Name: clusterfeatureworkflow.ClusterFeatureActivateActivityName})
+	}
+
+	{
+		a := clusterfeatureworkflow.MakeClusterFeatureDeleteActivity(featureRepository)
+		activity.RegisterWithOptions(a.Execute, activity.RegisterOptions{Name: clusterfeatureworkflow.ClusterFeatureDeleteActivityName})
+	}
+
+	{
+		a := clusterfeatureworkflow.MakeClusterFeatureDeactivateActivity(featureRegistry)
+		activity.RegisterWithOptions(a.Execute, activity.RegisterOptions{Name: clusterfeatureworkflow.ClusterFeatureDeactivateActivityName})
+	}
+
+	{
+		a := clusterfeatureworkflow.MakeClusterFeatureSetStatusActivity(featureRepository)
+		activity.RegisterWithOptions(a.Execute, activity.RegisterOptions{Name: clusterfeatureworkflow.ClusterFeatureSetStatusActivityName})
+	}
+
+	{
+		a := clusterfeatureworkflow.MakeClusterFeatureUpdateActivity(featureRegistry)
+		activity.RegisterWithOptions(a.Execute, activity.RegisterOptions{Name: clusterfeatureworkflow.ClusterFeatureUpdateActivityName})
+	}
+}
