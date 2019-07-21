e49068a42e1df09ddf2b8e9cf2e5f08740fba27f
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-07-21 12:09:11 +0200

    Add common logger interface

---
+++ internal/common/commonadapter/logger_test.go
@@ -0,0 +1,130 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package commonadapter
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/goph/logur"
+	"github.com/goph/logur/logtesting"
+)
+
+func TestLogger_Levels(t *testing.T) {
+	tests := map[string]struct {
+		logFunc func(logger *Logger, msg string, fields ...map[string]interface{})
+	}{
+		"trace": {
+			logFunc: (*Logger).Trace,
+		},
+		"debug": {
+			logFunc: (*Logger).Debug,
+		},
+		"info": {
+			logFunc: (*Logger).Info,
+		},
+		"warn": {
+			logFunc: (*Logger).Warn,
+		},
+		"error": {
+			logFunc: (*Logger).Error,
+		},
+	}
+
+	for name, test := range tests {
+		name, test := name, test
+
+		t.Run(name, func(t *testing.T) {
+			testLogger := logur.NewTestLogger()
+			logger := NewLogger(testLogger)
+
+			test.logFunc(logger, fmt.Sprintf("message: %s", name))
+
+			level, _ := logur.ParseLevel(name)
+
+			event := logur.LogEvent{
+				Level: level,
+				Line:  "message: " + name,
+			}
+
+			logtesting.AssertLogEventsEqual(t, event, *(testLogger.LastEvent()))
+		})
+	}
+}
+
+func TestLogger_WithFields(t *testing.T) {
+	testLogger := logur.NewTestLogger()
+
+	fields := map[string]interface{}{
+		"key1": "value1",
+		"key2": "value2",
+	}
+
+	logger := NewLogger(testLogger).WithFields(fields)
+
+	logger.Debug("message", nil)
+
+	event := logur.LogEvent{
+		Level:  logur.Debug,
+		Line:   "message",
+		Fields: fields,
+	}
+
+	logtesting.AssertLogEventsEqual(t, event, *(testLogger.LastEvent()))
+}
+
+type contextExtractor struct{}
+
+func (*contextExtractor) Extract(ctx context.Context) map[string]interface{} {
+	return map[string]interface{}{
+		"key1": "value1",
+		"key2": "value2",
+	}
+}
+
+func TestLogger_WithContext(t *testing.T) {
+	testLogger := logur.NewTestLogger()
+
+	logger := NewLogger(testLogger).WithContext(context.Background())
+
+	logger.Debug("message", nil)
+
+	event := logur.LogEvent{
+		Level: logur.Debug,
+		Line:  "message",
+	}
+
+	logtesting.AssertLogEventsEqual(t, event, *(testLogger.LastEvent()))
+}
+
+func TestContextAwareLogger_WithContext(t *testing.T) {
+	testLogger := logur.NewTestLogger()
+
+	logger := NewContextAwareLogger(testLogger, &contextExtractor{}).WithContext(context.Background())
+
+	logger.Debug("message", nil)
+
+	event := logur.LogEvent{
+		Level: logur.Debug,
+		Line:  "message",
+		Fields: map[string]interface{}{
+			"key1": "value1",
+			"key2": "value2",
+		},
+	}
+
+	logtesting.AssertLogEventsEqual(t, event, *(testLogger.LastEvent()))
+}
---
+++ internal/common/logger.go
@@ -0,0 +1,43 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package common
+
+import (
+	"context"
+)
+
+// Logger is the fundamental interface for all log operations.
+type Logger interface {
+	// Trace logs a debug event.
+	Trace(msg string, fields ...map[string]interface{})
+
+	// Debug logs a debug event.
+	Debug(msg string, fields ...map[string]interface{})
+
+	// Info logs an info event.
+	Info(msg string, fields ...map[string]interface{})
+
+	// Warn logs a warning event.
+	Warn(msg string, fields ...map[string]interface{})
+
+	// Error logs an error event.
+	Error(msg string, fields ...map[string]interface{})
+
+	// WithFields annotates a logger with key-value pairs.
+	WithFields(fields map[string]interface{}) Logger
+
+	// WithContext annotates a logger with a context.
+	WithContext(ctx context.Context) Logger
+}
