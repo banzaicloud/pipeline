ad624337ae494fa1649f31d6d3861ed3435f08b5
Mate Ory <mate.ory@banzaicloud.com>
2019-07-26 16:25:29 +0200

    k8sclient: deny connecting to local/private apiservers

--- pkg/k8sclient/client.go
+++ pkg/k8sclient/client.go
@@ -15,14 +15,21 @@
 package k8sclient
 
 import (
+	"context"
+	"net"
+
 	"emperror.dev/emperror"
 	"github.com/pkg/errors"
+	"github.com/spf13/viper"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
 // NewClient creates a new Kubernetes client from config.
 func NewClientFromConfig(config *rest.Config) (*kubernetes.Clientset, error) {
+	if viper.GetBool("pipeline.forceGlobal") {
+		config.Dial = blockerDial(config.Dial)
+	}
 	client, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		return nil, emperror.Wrap(err, "failed to create client for config")
@@ -49,4 +56,48 @@ func NewInClusterClient() (*kubernetes.Clientset, error) {
 	}
 
 	return NewClientFromConfig(config)
+}
+
+// globallyRoutable decides if an address is a globally routable IPv4 address
+func globallyRoutable(ip net.IP) bool {
+	if ip == nil || !ip.IsGlobalUnicast() {
+		return false
+	}
+	for _, cidr := range []string{"10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16"} {
+		_, net, _ := net.ParseCIDR(cidr)
+		if net.Contains(ip) {
+			return false
+		}
+	}
+	return true
+}
+
+type dialer func(context.Context, string, string) (net.Conn, error)
+
+// blockerDial wraps the default dialer but blocks connections to non-globally-routable addresses
+func blockerDial(original dialer) dialer {
+	return func(ctx context.Context, network, address string) (net.Conn, error) {
+		if original == nil {
+			dialer := &net.Dialer{}
+			original = dialer.DialContext
+		}
+		if address == "tcp" {
+			address = "tcp4" // TODO implement filtering for ipv6
+		}
+		conn, err := original(ctx, network, address)
+		if err == nil {
+			host, _, err := net.SplitHostPort(conn.RemoteAddr().String())
+			if err != nil {
+				conn.Close()
+				return nil, emperror.Wrap(err, "failed to parse remote address")
+			}
+			ip := net.ParseIP(host)
+			if ip == nil || !globallyRoutable(ip) {
+				conn.Close()
+				return nil, errors.New("remote address is not a global unicast address")
+			}
+		}
+
+		return conn, err
+	}
 }
