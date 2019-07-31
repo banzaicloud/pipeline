55a171ce69e9fe85b01729addc5191ec5c803f8a
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-08-01 01:40:45 +0200

    Add a common secret store interface and implementation

---
+++ internal/common/commonadapter/secret.go
@@ -0,0 +1,90 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package commonadapter
+
+import (
+	"context"
+
+	"emperror.dev/errors"
+
+	"github.com/banzaicloud/pipeline/internal/common"
+	"github.com/banzaicloud/pipeline/secret"
+)
+
+// SecretStore implements the common.SecretStore interface and acts as a lightweight wrapper around
+// the global secret store.
+type SecretStore struct {
+	store     OrganizationalSecretStore
+	extractor OrgIDContextExtractor
+}
+
+// OrganizationalSecretStore is the global secret store that stores values under a compound key:
+// the organization ID and a secret ID.
+type OrganizationalSecretStore interface {
+	// Get returns a secret in the internal format of the secret store.
+	Get(organizationID uint, secretID string) (*secret.SecretItemResponse, error)
+}
+
+// OrgIDContextExtractor extracts an organization ID from a context (if there is any).
+type OrgIDContextExtractor interface {
+	// GetOrganizationID extracts an organization ID from a context (if there is any).
+	GetOrganizationID(ctx context.Context) (uint, bool)
+}
+
+// OrgIDContextExtractorFunc converts an ordinary function to an OrgIDContextExtractor
+// (given it's method signature is compatible with the interface).
+type OrgIDContextExtractorFunc func(ctx context.Context) (uint, bool)
+
+// GetOrganizationID implements the OrgIDContextExtractor interface.
+func (f OrgIDContextExtractorFunc) GetOrganizationID(ctx context.Context) (uint, bool) {
+	return f(ctx)
+}
+
+// NewSecretStore returns a new SecretStore instance.
+func NewSecretStore(store OrganizationalSecretStore, extractor OrgIDContextExtractor) *SecretStore {
+	return &SecretStore{
+		store:     store,
+		extractor: extractor,
+	}
+}
+
+// GetSecretValues implements the common.SecretStore interface.
+func (s *SecretStore) GetSecretValues(ctx context.Context, secretID string) (map[string]string, error) {
+	organizationID, ok := s.extractor.GetOrganizationID(ctx)
+	if !ok {
+		return nil, errors.NewWithDetails(
+			"organization ID cannot be found in the context",
+			"organizationId", organizationID,
+			"secretId", secretID,
+		)
+	}
+
+	secretResponse, err := s.store.Get(organizationID, secretID)
+	if err == secret.ErrSecretNotExists {
+		return nil, errors.WithDetails(
+			errors.WithStack(common.SecretNotFoundError{SecretID: secretID}),
+			"organizationId", organizationID,
+		)
+	}
+	if err != nil {
+		return nil, errors.WithDetails(
+			errors.WithStackIf(err),
+			"organizationId", organizationID,
+			"secretId", secretID,
+		)
+	}
+
+	return secretResponse.Values, nil
+}
---
+++ internal/common/secret.go
@@ -0,0 +1,46 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package common
+
+import (
+	"context"
+)
+
+// SecretStore is a common interface for various parts of the application
+// to read secrets from the platform's secret store.
+//
+// It is not supposed to expose any implementation specific details.
+// If lower level access is required, use a different interface.
+type SecretStore interface {
+	// GetSecretValues returns the values stored within a secret.
+	// If the underlying store uses additional keys for determining the exact secret path
+	// (eg. organization ID), it should be retrieved from the context.
+	GetSecretValues(ctx context.Context, secretID string) (map[string]string, error)
+}
+
+// SecretNotFoundError is returned from a SecretStore if a secret cannot be found.
+type SecretNotFoundError struct {
+	SecretID string
+}
+
+// Error implements the builtin error interface.
+func (SecretNotFoundError) Error() string {
+	return "secret not found"
+}
+
+// Details returns details about the error in a generic, loggable format.
+func (e SecretNotFoundError) Details() []interface{} {
+	return []interface{}{"secretId", e.SecretID}
+}
