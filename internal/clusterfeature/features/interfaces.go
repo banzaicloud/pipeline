dec0c7cd791283628fb5f262225ec40c032d739c
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-08-01 03:18:50 +0200

    Replace feature secret store with a common one

---
+++ internal/clusterfeature/features/interfaces.go
@@ -0,0 +1,23 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package features
+
+import (
+	"github.com/banzaicloud/pipeline/internal/common"
+)
+
+// SecretStore is a common interface for various parts of the application
+// to read secrets from the platform's secret store.
+type SecretStore = common.SecretStore
