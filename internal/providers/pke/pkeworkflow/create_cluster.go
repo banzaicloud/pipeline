b34dbdd81011dc579ba880e1611af9742e65410a
Gabor Kozma <gabo@kozmagabor.com>
2019-08-23 15:57:48 +0200

    update pke tool version

--- internal/providers/pke/pkeworkflow/create_cluster.go
+++ internal/providers/pke/pkeworkflow/create_cluster.go
@@ -25,7 +25,7 @@ import (
 )
 
 const CreateClusterWorkflowName = "pke-create-cluster"
-const pkeVersion = "0.4.9"
+const pkeVersion = "0.4.12"
 
 func getDefaultImageID(region, kubernetesVersion string) (string, error) {
 	constraint112, err := semver.NewConstraint("~1.12.0")
