330ac6b7a918e5419698d94e2bb6abbeccc5b009
Nandor Kracser <bonifaido@gmail.com>
2019-09-04 15:41:40 +0200

    migrations: remove dex_enabled

---
+++ database/migrations/mysql/1571406333_drop_dex_enabled_field_to_amazon_ec2_clusters.down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `amazon_ec2_clusters` ADD COLUMN `dex_enabled` tinyint(1) DEFAULT 0 NOT NULL;
---
+++ database/migrations/postgres/1571406333_drop_dex_enabled_field_to_amazon_ec2_clusters.down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "amazon_ec2_clusters" ADD COLUMN "dex_enabled" boolean DEFAULT false NOT NULL;
