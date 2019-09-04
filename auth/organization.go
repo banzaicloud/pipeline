19579bdcdd28fddf2667c44b68e9835a878b3cea
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-09-04 23:41:04 +0200

    Add new organization syncer

--- auth/organization.go
+++ auth/organization.go
@@ -15,8 +15,11 @@
 package auth
 
 import (
+	"context"
 	"fmt"
 	"time"
+
+	"emperror.dev/errors"
 )
 
 // Organization represents a unit of users and resources.
@@ -34,4 +37,109 @@ type Organization struct {
 // IDString returns the ID as string.
 func (org *Organization) IDString() string {
 	return fmt.Sprint(org.ID)
+}
+
+// OrganizationSyncer synchronizes organization membership for a user.
+// It creates missing organizations, adds user to and removes from existing organizations,
+// updates organization role.
+// Note: it never deletes organizations, only creates them if they are missing.
+type OrganizationSyncer struct {
+	store OrganizationMembershipStore
+}
+
+// NewOrganizationSyncer returns a new OrganizationSyncer.
+func NewOrganizationSyncer(store OrganizationMembershipStore) OrganizationSyncer {
+	return OrganizationSyncer{
+		store: store,
+	}
+}
+
+// OrganizationMembershipStore is a persistence layer for organization membership.
+type OrganizationMembershipStore interface {
+	// EnsureOrganizationExists ensures that an organization exists.
+	// It also returns the fact that an organization was created or not.
+	EnsureOrganizationExists(ctx context.Context, organization UpstreamOrganization) error
+
+	// GetOrganizationMembershipsOf returns the list of organization memberships for a user.
+	GetOrganizationMembershipsOf(ctx context.Context, userID uint) ([]UserOrganization, error)
+
+	// RemoveFromOrganization removes a user from an organization.
+	RemoveFromOrganization(ctx context.Context, organizationID uint, userID uint) error
+
+	// UpdateUserMembership ensure that a user is a member of an organization with the necessary role.
+	UpdateUserMembership(ctx context.Context, organizationID uint, userID uint, role string) error
+
+	// AddUserTo ensure that a user is a member of an organization with the necessary role.
+	AddUserTo(ctx context.Context, organizationName string, userID uint, role string) error
+}
+
+// UpstreamOrganizationMembership represents an organization membership of a user
+// from the upstream authentication source.
+type UpstreamOrganizationMembership struct {
+	Organization UpstreamOrganization
+	Role         string
+}
+
+// UpstreamOrganization represents an organization from the upstream authentication source.
+type UpstreamOrganization struct {
+	Name     string
+	Provider string
+}
+
+// SyncOrganizations synchronizes organization membership for a user.
+func (s OrganizationSyncer) SyncOrganizations(ctx context.Context, user User, upstreamMemberships []UpstreamOrganizationMembership) error {
+	membershipsToAdd := make(map[string]string, len(upstreamMemberships))
+
+	for _, membership := range upstreamMemberships {
+		err := s.store.EnsureOrganizationExists(ctx, membership.Organization)
+		if err != nil {
+			return errors.WithDetails(err, "userId", user.ID)
+		}
+
+		membershipsToAdd[membership.Organization.Name] = membership.Role
+	}
+
+	currentMemberships, err := s.store.GetOrganizationMembershipsOf(ctx, user.ID)
+	if err != nil {
+		return err
+	}
+
+	for _, currentMembership := range currentMemberships {
+		role, ok := membershipsToAdd[currentMembership.Organization.Name]
+
+		// User is not in the list of upstream memberships, remove from organization
+		if !ok {
+			err := s.store.RemoveFromOrganization(ctx, currentMembership.OrganizationID, user.ID)
+			if err != nil {
+				return err
+			}
+
+			continue
+		}
+
+		// Membership is already up to date, there is nothing to do
+		if currentMembership.Role == role {
+			// Membership already exists, no need to add
+			delete(membershipsToAdd, currentMembership.Organization.Name)
+
+			continue
+		}
+
+		err := s.store.UpdateUserMembership(ctx, currentMembership.OrganizationID, user.ID, role)
+		if err != nil {
+			return err
+		}
+
+		// Membership already exists, no need to add
+		delete(membershipsToAdd, currentMembership.Organization.Name)
+	}
+
+	for organizationName, role := range membershipsToAdd {
+		err := s.store.AddUserTo(ctx, organizationName, user.ID, role)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
---
+++ auth/organization_test.go
@@ -0,0 +1,128 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package auth
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+//go:generate sh -c "test -x ${MOCKERY} && ${MOCKERY} -name OrganizationMembershipStore -inpkg -testonly"
+
+func TestOrganizationSyncer_SyncOrganizations(t *testing.T) { // TODO: rewrite this test with an in-memory store
+	store := &MockOrganizationMembershipStore{}
+	syncer := NewOrganizationSyncer(store)
+
+	ctx := context.Background()
+
+	user := User{
+		ID: 1,
+	}
+
+	upstreamMemberships := []UpstreamOrganizationMembership{
+		{
+			Organization: UpstreamOrganization{
+				Name:     "stays-the-same",
+				Provider: "github",
+			},
+			Role: RoleAdmin,
+		},
+		{
+			Organization: UpstreamOrganization{
+				Name:     "change-role-to-member",
+				Provider: "github",
+			},
+			Role: RoleMember,
+		},
+		{
+			Organization: UpstreamOrganization{
+				Name:     "change-role-to-admin",
+				Provider: "github",
+			},
+			Role: RoleAdmin,
+		},
+		{
+			Organization: UpstreamOrganization{
+				Name:     "new-org",
+				Provider: "github",
+			},
+			Role: RoleAdmin,
+		},
+	}
+
+	for _, upstreamMembership := range upstreamMemberships {
+		store.On("EnsureOrganizationExists", ctx, upstreamMembership.Organization).Return(nil)
+	}
+
+	currentMemberships := []UserOrganization{
+		{
+			User:   user,
+			UserID: user.ID,
+			Organization: Organization{
+				ID:       1,
+				Name:     "stays-the-same",
+				Provider: "github",
+			},
+			OrganizationID: 1,
+			Role:           RoleAdmin,
+		},
+		{
+			User:   user,
+			UserID: user.ID,
+			Organization: Organization{
+				ID:       2,
+				Name:     "change-role-to-member",
+				Provider: "github",
+			},
+			OrganizationID: 2,
+			Role:           RoleAdmin,
+		},
+		{
+			User:   user,
+			UserID: user.ID,
+			Organization: Organization{
+				ID:       3,
+				Name:     "change-role-to-admin",
+				Provider: "github",
+			},
+			OrganizationID: 3,
+			Role:           RoleMember,
+		},
+		{
+			User:   user,
+			UserID: user.ID,
+			Organization: Organization{
+				ID:       4,
+				Name:     "lose-access",
+				Provider: "github",
+			},
+			OrganizationID: 4,
+			Role:           RoleAdmin,
+		},
+	}
+
+	store.On("GetOrganizationMembershipsOf", ctx, user.ID).Return(currentMemberships, nil)
+	store.On("RemoveFromOrganization", ctx, currentMemberships[3].OrganizationID, user.ID).Return(nil)
+	store.On("UpdateUserMembership", ctx, currentMemberships[1].OrganizationID, user.ID, RoleMember).Return(nil)
+	store.On("UpdateUserMembership", ctx, currentMemberships[2].OrganizationID, user.ID, RoleAdmin).Return(nil)
+	store.On("AddUserTo", ctx, upstreamMemberships[3].Organization.Name, user.ID, RoleAdmin).Return(nil)
+
+	err := syncer.SyncOrganizations(ctx, user, upstreamMemberships)
+	require.NoError(t, err)
+
+	store.AssertExpectations(t)
+}
