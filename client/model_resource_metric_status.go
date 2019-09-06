18142989e88a64eb4df0167b68a039e7375751ec
Varga Zsolt <waynz0r@users.noreply.github.com>
2019-09-06 18:15:11 +0200

    change pipeline.yaml version to latest (#2285)
    * change pipeline.yaml description and also version to latest

--- client/model_resource_metric_status.go
+++ client/model_resource_metric_status.go
@@ -1,9 +1,9 @@
 /*
  * Pipeline API
  *
- * Pipeline v0.3.0 swagger
+ * Pipeline is a feature rich application platform, built for containers on top of Kubernetes to automate the DevOps experience, continuous application development and the lifecycle of deployments.
  *
- * API version: 0.3.0
+ * API version: latest
  * Contact: info@banzaicloud.com
  */
 
