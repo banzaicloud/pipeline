10dd945ea013b192d2918f64a0116a4f694bc705
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2019-10-25 16:32:35 +0200

    Refactor cluster feature api integration

--- internal/clusterfeature/clusterfeaturedriver/transport_http_test.go
+++ internal/clusterfeature/clusterfeaturedriver/transport_http_test.go
@@ -16,61 +16,23 @@ package clusterfeaturedriver
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"emperror.dev/emperror"
+	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/banzaicloud/pipeline/.gen/pipeline/pipeline"
-	"github.com/banzaicloud/pipeline/internal/clusterfeature"
 	"github.com/banzaicloud/pipeline/pkg/ctxutil"
 )
 
-func TestMakeHTTPHandlers_List(t *testing.T) {
-	featureService := &dummyFeatureService{
-		FeatureList: []clusterfeature.Feature{
-			{
-				Name: "example",
-				Spec: map[string]interface{}{
-					"hello": "world",
-				},
-				Output: map[string]interface{}{
-					"hello": "world",
-				},
-				Status: "ACTIVE",
-			},
-		},
-	}
-
-	handler := MakeHTTPHandlers(MakeEndpoints(featureService), emperror.NewNoopHandler()).List
-
-	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r = r.WithContext(ctxutil.WithClusterID(r.Context(), 1))
-
-		handler.ServeHTTP(w, r)
-	}))
-	defer ts.Close()
-
-	tsClient := ts.Client()
-
-	resp, err := tsClient.Get(ts.URL)
-	require.NoError(t, err)
-	defer resp.Body.Close()
-
-	assert.Equal(t, resp.StatusCode, http.StatusOK)
-
-	decoder := json.NewDecoder(resp.Body)
-
-	var featureMap map[string]pipeline.ClusterFeatureDetails
-
-	err = decoder.Decode(&featureMap)
-	require.NoError(t, err)
-
-	assert.Equal(t, map[string]pipeline.ClusterFeatureDetails{
+func TestRegisterHTTPHandlers_List(t *testing.T) {
+	expectedFeatures := map[string]pipeline.ClusterFeatureDetails{
 		"example": {
 			Status: "ACTIVE",
 			Spec: map[string]interface{}{
@@ -80,53 +42,42 @@ func TestMakeHTTPHandlers_List(t *testing.T) {
 				"hello": "world",
 			},
 		},
-	}, featureMap)
-}
-
-func TestMakeHTTPHandlers_Details(t *testing.T) {
-	featureService := &dummyFeatureService{
-		FeatureDetails: clusterfeature.Feature{
-			Name: "example",
-			Spec: map[string]interface{}{
-				"hello": "world",
-			},
-			Output: map[string]interface{}{
-				"hello": "world",
-			},
-			Status: "ACTIVE",
-		},
 	}
 
-	handler := MakeHTTPHandlers(MakeEndpoints(featureService), emperror.NewNoopHandler()).Details
+	handler := mux.NewRouter()
+	RegisterHTTPHandlers(
+		Endpoints{
+			List: func(ctx context.Context, request interface{}) (response interface{}, err error) {
+				return expectedFeatures, nil
+			},
+		},
+		handler.PathPrefix("/features").Subrouter(),
+		emperror.NewNoopHandler(),
+	)
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r = r.WithContext(ctxutil.WithParams(
-			ctxutil.WithClusterID(r.Context(), 1),
-			map[string]string{
-				"featureName": "example",
-			},
-		))
+		r = r.WithContext(ctxutil.WithClusterID(r.Context(), 1))
 
 		handler.ServeHTTP(w, r)
 	}))
 	defer ts.Close()
 
-	tsClient := ts.Client()
-
-	resp, err := tsClient.Get(ts.URL)
+	resp, err := ts.Client().Get(ts.URL + "/features")
 	require.NoError(t, err)
 	defer resp.Body.Close()
 
-	assert.Equal(t, resp.StatusCode, http.StatusOK)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
 
-	decoder := json.NewDecoder(resp.Body)
+	var featureMap map[string]pipeline.ClusterFeatureDetails
 
-	var featureDetails pipeline.ClusterFeatureDetails
-
-	err = decoder.Decode(&featureDetails)
+	err = json.NewDecoder(resp.Body).Decode(&featureMap)
 	require.NoError(t, err)
 
-	assert.Equal(t, pipeline.ClusterFeatureDetails{
+	assert.Equal(t, expectedFeatures, featureMap)
+}
+
+func TestRegisterHTTPHandlers_Details(t *testing.T) {
+	expectedDetails := pipeline.ClusterFeatureDetails{
 		Spec: map[string]interface{}{
 			"hello": "world",
 		},
@@ -134,31 +85,60 @@ func TestMakeHTTPHandlers_Details(t *testing.T) {
 			"hello": "world",
 		},
 		Status: "ACTIVE",
-	}, featureDetails)
-}
+	}
 
-func TestMakeHTTPHandlers_Activate(t *testing.T) {
-	featureService := &dummyFeatureService{}
-
-	handler := MakeHTTPHandlers(MakeEndpoints(featureService), emperror.NewNoopHandler()).Activate
+	handler := mux.NewRouter()
+	RegisterHTTPHandlers(
+		Endpoints{
+			Details: func(ctx context.Context, request interface{}) (response interface{}, err error) {
+				return expectedDetails, nil
+			},
+		},
+		handler.PathPrefix("/features").Subrouter(),
+		emperror.NewNoopHandler(),
+	)
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r = r.WithContext(ctxutil.WithParams(
-			ctxutil.WithClusterID(r.Context(), 1),
-			map[string]string{
-				"featureName": "example",
-			},
-		))
+		r = r.WithContext(ctxutil.WithClusterID(r.Context(), 1))
+		r = r.WithContext(ctxutil.WithParams(r.Context(), map[string]string{"featureName": "hello-world"}))
 
 		handler.ServeHTTP(w, r)
 	}))
 	defer ts.Close()
 
-	tsClient := ts.Client()
+	resp, err := ts.Client().Get(ts.URL + "/features/hello-world")
+	require.NoError(t, err)
+	defer resp.Body.Close()
 
-	var buf bytes.Buffer
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
 
-	encoder := json.NewEncoder(&buf)
+	var featureDetails pipeline.ClusterFeatureDetails
+
+	err = json.NewDecoder(resp.Body).Decode(&featureDetails)
+	require.NoError(t, err)
+
+	assert.Equal(t, expectedDetails, featureDetails)
+}
+
+func TestRegisterHTTPHandlers_Activate(t *testing.T) {
+	handler := mux.NewRouter()
+	RegisterHTTPHandlers(
+		Endpoints{
+			Activate: func(ctx context.Context, request interface{}) (response interface{}, err error) {
+				return nil, nil
+			},
+		},
+		handler.PathPrefix("/features").Subrouter(),
+		emperror.NewNoopHandler(),
+	)
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		r = r.WithContext(ctxutil.WithClusterID(r.Context(), 1))
+		r = r.WithContext(ctxutil.WithParams(r.Context(), map[string]string{"featureName": "hello-world"}))
+
+		handler.ServeHTTP(w, r)
+	}))
+	defer ts.Close()
 
 	apiReq := pipeline.ActivateClusterFeatureRequest{
 		Spec: map[string]interface{}{
@@ -166,70 +146,65 @@ func TestMakeHTTPHandlers_Activate(t *testing.T) {
 		},
 	}
 
-	err := encoder.Encode(&apiReq)
+	body, err := json.Marshal(apiReq)
 	require.NoError(t, err)
 
-	req, err := http.NewRequest(http.MethodPost, ts.URL, &buf)
-	require.NoError(t, err)
-
-	resp, err := tsClient.Do(req)
+	resp, err := ts.Client().Post(ts.URL+"/features/hello-world", "application/json", bytes.NewReader(body))
 	require.NoError(t, err)
 	defer resp.Body.Close()
 
-	assert.Equal(t, resp.StatusCode, http.StatusAccepted)
+	assert.Equal(t, http.StatusAccepted, resp.StatusCode)
 }
 
-func TestMakeHTTPHandlers_Deactivate(t *testing.T) {
-	featureService := &dummyFeatureService{}
-
-	handler := MakeHTTPHandlers(MakeEndpoints(featureService), emperror.NewNoopHandler()).Deactivate
+func TestRegisterHTTPHandlers_Deactivate(t *testing.T) {
+	handler := mux.NewRouter()
+	RegisterHTTPHandlers(
+		Endpoints{
+			Deactivate: func(ctx context.Context, request interface{}) (response interface{}, err error) {
+				return nil, nil
+			},
+		},
+		handler.PathPrefix("/features").Subrouter(),
+		emperror.NewNoopHandler(),
+	)
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r = r.WithContext(ctxutil.WithParams(
-			ctxutil.WithClusterID(r.Context(), 1),
-			map[string]string{
-				"featureName": "example",
-			},
-		))
+		r = r.WithContext(ctxutil.WithClusterID(r.Context(), 1))
+		r = r.WithContext(ctxutil.WithParams(r.Context(), map[string]string{"featureName": "hello-world"}))
 
 		handler.ServeHTTP(w, r)
 	}))
 	defer ts.Close()
 
-	client := ts.Client()
-
-	req, err := http.NewRequest(http.MethodDelete, ts.URL, nil)
+	req, err := http.NewRequest(http.MethodDelete, ts.URL+"/features/hello-world", nil)
 	require.NoError(t, err)
 
-	resp, err := client.Do(req)
+	resp, err := ts.Client().Do(req)
 	require.NoError(t, err)
 	defer resp.Body.Close()
 
-	assert.Equal(t, resp.StatusCode, http.StatusNoContent)
+	assert.Equal(t, http.StatusNoContent, resp.StatusCode)
 }
 
-func TestMakeHTTPHandlers_Update(t *testing.T) {
-	featureService := &dummyFeatureService{}
-
-	handler := MakeHTTPHandlers(MakeEndpoints(featureService), emperror.NewNoopHandler()).Update
+func TestRegisterHTTPHandlers_Update(t *testing.T) {
+	handler := mux.NewRouter()
+	RegisterHTTPHandlers(
+		Endpoints{
+			Update: func(ctx context.Context, request interface{}) (response interface{}, err error) {
+				return nil, nil
+			},
+		},
+		handler.PathPrefix("/features").Subrouter(),
+		emperror.NewNoopHandler(),
+	)
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		r = r.WithContext(ctxutil.WithParams(
-			ctxutil.WithClusterID(r.Context(), 1),
-			map[string]string{
-				"featureName": "example",
-			},
-		))
+		r = r.WithContext(ctxutil.WithClusterID(r.Context(), 1))
+		r = r.WithContext(ctxutil.WithParams(r.Context(), map[string]string{"featureName": "hello-world"}))
 
 		handler.ServeHTTP(w, r)
 	}))
 	defer ts.Close()
-
-	tsClient := ts.Client()
-
-	var buf bytes.Buffer
-
-	encoder := json.NewEncoder(&buf)
 
 	apiReq := pipeline.UpdateClusterFeatureRequest{
 		Spec: map[string]interface{}{
@@ -237,15 +212,15 @@ func TestMakeHTTPHandlers_Update(t *testing.T) {
 		},
 	}
 
-	err := encoder.Encode(&apiReq)
+	body, err := json.Marshal(apiReq)
 	require.NoError(t, err)
 
-	req, err := http.NewRequest(http.MethodPut, ts.URL, &buf)
+	req, err := http.NewRequest(http.MethodPut, ts.URL+"/features/hello-world", bytes.NewReader(body))
 	require.NoError(t, err)
 
-	resp, err := tsClient.Do(req)
+	resp, err := ts.Client().Do(req)
 	require.NoError(t, err)
 	defer resp.Body.Close()
 
-	assert.Equal(t, resp.StatusCode, http.StatusAccepted)
+	assert.Equal(t, http.StatusAccepted, resp.StatusCode)
 }
