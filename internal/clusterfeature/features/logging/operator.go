ddab803cbc97555ae8a053819810a5db0acc31a8
colin <kispiszkerf@gmail.com>
2019-11-15 16:55:11 +0100

    monitoring: set hosts fields only in case of Loki ingress

--- internal/clusterfeature/features/logging/operator.go
+++ internal/clusterfeature/features/logging/operator.go
@@ -17,6 +17,7 @@ package logging
 import (
 	"context"
 	"encoding/json"
+	"path"
 
 	"emperror.dev/errors"
 	"github.com/mitchellh/copystructure"
@@ -354,11 +355,15 @@ func (op FeatureOperator) processLoki(ctx context.Context, spec lokiSpec, cl clu
 			annotations = generateAnnotations(secretName)
 		}
 
+		var domain = spec.Ingress.Domain
+		if domain == "" {
+			domain = "/"
+		}
+
 		var chartValues = &lokiValues{
 			Ingress: ingressValues{
 				Enabled: spec.Ingress.Enabled,
-				Hosts:   []string{spec.Ingress.Domain},
-				Path:    spec.Ingress.Path,
+				Hosts:   []string{path.Join(domain, spec.Ingress.Path)},
 			},
 			Annotations: annotations,
 			Image: imageValues{
