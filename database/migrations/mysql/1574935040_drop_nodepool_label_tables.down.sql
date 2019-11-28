875dd98cb10f23165493d8cf9a2376231f4752cb
Toader, Sebastian <st0ad3r@gmail.com>
2019-11-28 13:57:26 +0100

    fix identation

--- database/migrations/mysql/1574935040_drop_nodepool_label_tables.down.sql
+++ database/migrations/mysql/1574935040_drop_nodepool_label_tables.down.sql
@@ -1,32 +1,32 @@
 CREATE TABLE `oracle_oke_node_pool_labels` (
-                                               `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-                                               `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-                                               `value` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-                                               `node_pool_id` int(10) unsigned DEFAULT NULL,
-                                               `created_at` timestamp NULL DEFAULT NULL,
-                                               `updated_at` timestamp NULL DEFAULT NULL,
-                                               PRIMARY KEY (`id`),
-                                               UNIQUE KEY `idx_node_pool_id_name` (`name`,`node_pool_id`)
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `value` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `node_pool_id` int(10) unsigned DEFAULT NULL,
+    `created_at` timestamp NULL DEFAULT NULL,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_node_pool_id_name` (`name`,`node_pool_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `amazon_node_pool_labels` (
-                                           `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-                                           `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-                                           `value` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-                                           `node_pool_id` int(10) unsigned DEFAULT NULL,
-                                           `created_at` timestamp NULL DEFAULT NULL,
-                                           `updated_at` timestamp NULL DEFAULT NULL,
-                                           PRIMARY KEY (`id`),
-                                           UNIQUE KEY `idx_node_pool_id_name` (`name`,`node_pool_id`)
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `value` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `node_pool_id` int(10) unsigned DEFAULT NULL,
+    `created_at` timestamp NULL DEFAULT NULL,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_node_pool_id_name` (`name`,`node_pool_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 CREATE TABLE `google_gke_node_pool_labels` (
-                                               `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-                                               `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-                                               `value` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-                                               `node_pool_id` int(10) unsigned DEFAULT NULL,
-                                               `created_at` timestamp NULL DEFAULT NULL,
-                                               `updated_at` timestamp NULL DEFAULT NULL,
-                                               PRIMARY KEY (`id`),
-                                               UNIQUE KEY `idx_node_pool_id_name` (`name`,`node_pool_id`)
+    `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `value` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    `node_pool_id` int(10) unsigned DEFAULT NULL,
+    `created_at` timestamp NULL DEFAULT NULL,
+    `updated_at` timestamp NULL DEFAULT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `idx_node_pool_id_name` (`name`,`node_pool_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
