2d058e7088be22aac33197b39f3cf86ff5a209e4
Zsolt Varga <zsolt.varga@console.hu>
2020-01-28 22:29:46 +0100

    make linter happy

--- internal/istio/istiofeature/reconcile_remoteistio_cr.go
+++ internal/istio/istiofeature/reconcile_remoteistio_cr.go
@@ -20,13 +20,13 @@ import (
 	"time"
 
 	"emperror.dev/errors"
+	"github.com/banzaicloud/istio-operator/pkg/apis/istio/v1beta1"
 	corev1 "k8s.io/api/core/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/banzaicloud/istio-operator/pkg/apis/istio/v1beta1"
 	"github.com/banzaicloud/pipeline/pkg/backoff"
 	"github.com/banzaicloud/pipeline/src/cluster"
 )
