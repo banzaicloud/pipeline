1f72f2d6205b532950832921637dd1752f19e8b7
Laszlo Puskas <laszlo@banzaicloud.com>
2020-02-21 15:01:47 +0100

    add initial transport layer to the service

---
+++ internal/helm3/helm3driver/transport_http.go
@@ -0,0 +1,103 @@
+// Copyright © 2020 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package helm3driver
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"emperror.dev/errors"
+	kithttp "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+	kitxhttp "github.com/sagikazarmark/kitx/transport/http"
+
+	apphttp "github.com/banzaicloud/pipeline/internal/platform/appkit/transport/http"
+)
+
+func RegisterHTTPHandlers(endpoints Endpoints, router *mux.Router, options ...kithttp.ServerOption) {
+	errorEncoder := kitxhttp.NewJSONProblemErrorResponseEncoder(apphttp.NewDefaultProblemConverter())
+
+	router.Methods(http.MethodPost).Path("").Handler(kithttp.NewServer(
+		endpoints.AddRepository,
+		decodeAddRepositoryHTTPRequest,
+		kitxhttp.ErrorResponseEncoder(encodeCreateTokenHTTPResponse, errorEncoder),
+		options...,
+	))
+
+	router.Methods(http.MethodGet).Path("").Handler(kithttp.NewServer(
+		endpoints.ListRepositories,
+		decodeListRepositoryHTTPRequest,
+		kitxhttp.ErrorResponseEncoder(encodeListRepositoriesHTTPResponse, errorEncoder),
+		options...,
+	))
+}
+
+func decodeAddRepositoryHTTPRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	orgID, e := extractOrgID(r)
+	if e != nil {
+		return nil, errors.WrapIff(e, "failed to decode add repository request")
+	}
+
+	var addRepositoryRequest AddRepositoryRequest
+
+	err := json.NewDecoder(r.Body).Decode(&addRepositoryRequest)
+	if err != nil {
+		return nil, errors.WrapIf(err, "failed to decode request")
+	}
+
+	addRepositoryRequest.OrganizationID = orgID
+
+	return addRepositoryRequest, nil
+}
+
+func encodeCreateTokenHTTPResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	resp := response.(AddRepositoryResponse)
+
+	return kitxhttp.JSONResponseEncoder(ctx, w, resp)
+}
+
+func decodeListRepositoryHTTPRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	orgID, err := extractOrgID(r)
+	if err != nil {
+		return 0, errors.WrapIf(err, "failed to decode list request")
+	}
+
+	return ListRepositoriesRequest{OrganizationID: orgID}, nil
+
+}
+
+func encodeListRepositoriesHTTPResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	resp := response.(ListRepositoriesResponse)
+
+	return kitxhttp.JSONResponseEncoder(ctx, w, resp.Repos)
+}
+
+func extractOrgID(r *http.Request) (uint, error) {
+	vars := mux.Vars(r)
+
+	id, ok := vars["orgId"]
+	if !ok || id == "" {
+		return 0, errors.NewWithDetails("missing path parameter", "param", "orgId")
+	}
+
+	orgID, e := strconv.ParseUint(id, 10, 32)
+	if e != nil {
+		return 0, errors.WrapIff(e, "failed to parse path param: %s, value:  %s", "id", id)
+	}
+
+	return uint(orgID), nil
+}
