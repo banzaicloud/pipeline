9e0467ee09b0612d969d29cca0bb04723f044f56
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2020-02-29 02:13:32 +0100

    Add PagerDuty secret type

---
+++ internal/secret/types/type_pagerduty.go
@@ -0,0 +1,43 @@
+// Copyright © 2020 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package types
+
+import (
+	"github.com/banzaicloud/pipeline/internal/secret"
+)
+
+const PagerDuty = "pagerduty"
+
+const (
+	FieldPagerDutyIntegrationKey = "integrationKey"
+)
+
+type PagerDutyType struct{}
+
+func (PagerDutyType) Name() string {
+	return PagerDuty
+}
+
+func (PagerDutyType) Definition() secret.TypeDefinition {
+	return secret.TypeDefinition{
+		Fields: []secret.FieldDefinition{
+			{Name: FieldPagerDutyIntegrationKey, Required: true, Opaque: true, Description: "The PagerDuty integration key"},
+		},
+	}
+}
+
+func (t PagerDutyType) Validate(data map[string]string) error {
+	return validateDefinition(data, t.Definition())
+}
