3e501354f02e020c62cd6b2ee2adef9ed269b584
Laszlo Puskas <laszlo@banzaicloud.com>
2020-04-21 15:55:27 +0200

    ensure helm environment (helm 3) / add (all) default repos

--- internal/helm/helmadapter/helm2_env_service.go
+++ internal/helm/helmadapter/helm2_env_service.go
@@ -175,7 +175,7 @@ func (h helmEnvService) repositoryToEntry(repository helm.Repository) (repo.Entr
 	return entry, nil
 }
 
-func (h helmEnvService) EnsureEnv(_ context.Context, helmEnv helm.HelmEnv) (helm.HelmEnv, error) {
+func (h helmEnvService) EnsureEnv(_ context.Context, helmEnv helm.HelmEnv, defaultRepos []helm.Repository) (helm.HelmEnv, error) {
 	// no op implementation / helm 2
 	return helmEnv, nil
 }
