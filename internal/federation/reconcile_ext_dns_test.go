cc999a89d2fa101476524f4d1fe7f8988b06aad8
pepov <peterwilcsinszky@gmail.com>
2020-05-07 11:09:27 +0200

    add integration test wrapper and add v3 test case

--- internal/federation/reconcile_ext_dns_test.go
+++ internal/federation/reconcile_ext_dns_test.go
@@ -24,99 +24,101 @@ import (
 	internaltesting "github.com/banzaicloud/pipeline/internal/testing"
 )
 
-func TestEnsureCRDSourceForExtDNS(t *testing.T) {
-	testNamespace := "test-fed-ext-dns"
-	chartName := "stable/external-dns"
-	releaseName := "fed-ext-dns"
-	chartVersion := "2.15.2"
+func testEnsureCRDSourceForExtDNS(v3 bool) func(t *testing.T) {
+	return func(t *testing.T) {
+		testNamespace := "test-fed-ext-dns"
+		chartName := "stable/external-dns"
+		releaseName := "fed-ext-dns"
+		chartVersion := "2.15.2"
 
-	org := uint(0)
-	clusterId := uint(1)
+		org := uint(0)
+		clusterId := uint(1)
 
-	kubeConfig, clusterService := helmtesting.ClusterKubeConfig(t, clusterId)
+		kubeConfig, clusterService := helmtesting.ClusterKubeConfig(t, clusterId)
 
-	clusterConfig := internaltesting.ClusterData{
-		K8sConfig: kubeConfig,
-		ID:        org,
-	}
+		clusterConfig := internaltesting.ClusterData{
+			K8sConfig: kubeConfig,
+			ID:        org,
+		}
 
-	db := helmtesting.SetupDatabase(t)
-	secretStore := helmtesting.SetupSecretStore()
-	helmLogger, logrusLogger := helmtesting.Loggers()
+		db := helmtesting.SetupDatabase(t)
+		secretStore := helmtesting.SetupSecretStore()
+		helmLogger, logrusLogger := helmtesting.Loggers()
 
-	home := helmtesting.HelmHome(t)
+		home := helmtesting.HelmHome(t)
 
-	config := helm.Config{
-		Home: home,
-		V3:   false,
-		Repositories: map[string]string{
-			"stable": "https://kubernetes-charts.storage.googleapis.com",
-		},
-	}
+		config := helm.Config{
+			Home: home,
+			V3:   v3,
+			Repositories: map[string]string{
+				"stable": "https://kubernetes-charts.storage.googleapis.com",
+			},
+		}
 
-	orgService := helmtesting.FakeOrg{
-		OrgId:   0,
-		OrgName: "",
-	}
+		orgService := helmtesting.FakeOrg{
+			OrgId:   0,
+			OrgName: "",
+		}
 
-	unifiedReleaser, _ := cmd.CreateUnifiedHelmReleaser(config, db, secretStore, clusterService, orgService, helmLogger)
+		unifiedReleaser, _ := cmd.CreateUnifiedHelmReleaser(config, db, secretStore, clusterService, orgService, helmLogger)
 
-	if err := unifiedReleaser.Delete(&clusterConfig, releaseName, testNamespace); err != nil {
-		t.Fatalf("%+v", err)
-	}
-	err := unifiedReleaser.InstallOrUpgrade(&clusterConfig, helm.Release{
-		ReleaseName: releaseName,
-		ChartName:   chartName,
-		Namespace:   testNamespace,
-		Values:      nil,
-		Version:     chartVersion,
-	}, helm.Options{
-		Namespace: testNamespace,
-		Wait:      true,
-	})
-	if err != nil {
-		t.Fatalf("%+v", err)
-	}
+		if err := unifiedReleaser.Delete(&clusterConfig, releaseName, testNamespace); err != nil {
+			t.Fatalf("%+v", err)
+		}
+		err := unifiedReleaser.InstallOrUpgrade(&clusterConfig, helm.Release{
+			ReleaseName: releaseName,
+			ChartName:   chartName,
+			Namespace:   testNamespace,
+			Values:      nil,
+			Version:     chartVersion,
+		}, helm.Options{
+			Namespace: testNamespace,
+			Wait:      true,
+		})
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
 
-	var desiredState federation.DesiredState
+		var desiredState federation.DesiredState
 
-	desiredState = federation.DesiredStateAbsent
-	upgraded, err := federation.EnsureCRDSourceForExtDNS(&clusterConfig, testNamespace, chartName, releaseName, desiredState, logrusLogger)
-	if err != nil {
-		t.Fatalf("%+v", err)
-	}
-	if upgraded {
-		t.Fatal("expected the chart not to be upgraded")
-	}
+		desiredState = federation.DesiredStateAbsent
+		upgraded, err := federation.EnsureCRDSourceForExtDNS(&clusterConfig, testNamespace, chartName, releaseName, desiredState, logrusLogger)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+		if upgraded {
+			t.Fatal("expected the chart not to be upgraded")
+		}
 
-	desiredState = federation.DesiredStatePresent
-	upgraded, err = federation.EnsureCRDSourceForExtDNS(&clusterConfig, testNamespace, chartName, releaseName, desiredState, logrusLogger)
-	if err != nil {
-		t.Fatalf("%+v", err)
-	}
-	if !upgraded {
-		t.Fatal("expected the chart has been upgraded")
-	}
+		desiredState = federation.DesiredStatePresent
+		upgraded, err = federation.EnsureCRDSourceForExtDNS(&clusterConfig, testNamespace, chartName, releaseName, desiredState, logrusLogger)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+		if !upgraded {
+			t.Fatal("expected the chart has been upgraded")
+		}
 
-	desiredState = federation.DesiredStatePresent
-	upgraded, err = federation.EnsureCRDSourceForExtDNS(&clusterConfig, testNamespace, chartName, releaseName, desiredState, logrusLogger)
-	if err != nil {
-		t.Fatalf("%+v", err)
-	}
-	if upgraded {
-		t.Fatal("expected the chart not to be upgraded the second time")
-	}
+		desiredState = federation.DesiredStatePresent
+		upgraded, err = federation.EnsureCRDSourceForExtDNS(&clusterConfig, testNamespace, chartName, releaseName, desiredState, logrusLogger)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+		if upgraded {
+			t.Fatal("expected the chart not to be upgraded the second time")
+		}
 
-	desiredState = federation.DesiredStateAbsent
-	upgraded, err = federation.EnsureCRDSourceForExtDNS(&clusterConfig, testNamespace, chartName, releaseName, desiredState, logrusLogger)
-	if err != nil {
-		t.Fatalf("%+v", err)
-	}
-	if !upgraded {
-		t.Fatal("expected the chart not to be upgraded")
-	}
+		desiredState = federation.DesiredStateAbsent
+		upgraded, err = federation.EnsureCRDSourceForExtDNS(&clusterConfig, testNamespace, chartName, releaseName, desiredState, logrusLogger)
+		if err != nil {
+			t.Fatalf("%+v", err)
+		}
+		if !upgraded {
+			t.Fatal("expected the chart not to be upgraded")
+		}
 
-	if err := unifiedReleaser.Delete(&clusterConfig, releaseName, testNamespace); err != nil {
-		t.Fatalf("%+v", err)
+		if err := unifiedReleaser.Delete(&clusterConfig, releaseName, testNamespace); err != nil {
+			t.Fatalf("%+v", err)
+		}
 	}
 }
