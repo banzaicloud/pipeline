5c62398379dd2f2f028c973bdc4d1a3a1d1580aa
pepov <peterwilcsinszky@gmail.com>
2020-05-12 16:00:29 +0200

    upgrade helm and kubernetes patch versions

--- cmd/pipeline/capabilities.go
+++ cmd/pipeline/capabilities.go
@@ -49,7 +49,7 @@ func mapCapabilities(config configuration) cap.Capabilities {
 			},
 		},
 		"helm": cap.Cap{
-			"version": "3.1.2", // TODO: determine based on go.mod build time
+			"version": "3.1.3", // TODO: determine based on go.mod build time
 		},
 	}
 }
