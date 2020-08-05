95728e9070f229b6ad4f215c375a7fd731e1c67c
Patrik Egyed <patrik.egyed@banzaicloud.com>
2020-08-05 15:47:15 +0200

    fix(oidc): downgraded TLS to 1.2
    Dex doesn't support TLS 1.3 yet.

--- src/auth/oidc.go
+++ src/auth/oidc.go
@@ -92,7 +92,7 @@ func newOIDCProvider(config *OIDCProviderConfig, refreshTokenStore RefreshTokenS
 		Transport: &http.Transport{
 			TLSClientConfig: &tls.Config{
 				InsecureSkipVerify: config.InsecureSkipVerify,
-				MinVersion:         tls.VersionTLS13,
+				MinVersion:         tls.VersionTLS12,
 			},
 		},
 	}
