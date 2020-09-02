148dc74b39091d3c2cec4854e507965e5d51676c
Patrik Egyed <patrik.egyed@banzaicloud.com>
2020-09-02 09:50:44 +0200

    feat(EKS): implemented volume size activities

---
+++ internal/cluster/distribution/eks/eksprovider/workflow/get_ami_size_activity.go
@@ -0,0 +1,75 @@
+// Copyright © 2020 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package workflow
+
+import (
+	"context"
+
+	"emperror.dev/errors"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+const (
+	GetAMISizeActivityName = "eks-get-ami-size-activity"
+)
+
+type GetAMISizeActivity struct {
+	awsFactory AWSFactory
+	ec2Factory EC2APIFactory
+}
+
+type GetAMISizeActivityInput struct {
+	EKSActivityInput
+	ImageID string
+}
+
+type GetAMISizeActivityOutput struct {
+	AMISize int
+}
+
+func NewGetAMISizeActivity(awsFactory AWSFactory, ec2Factory EC2APIFactory) *GetAMISizeActivity {
+	return &GetAMISizeActivity{
+		awsFactory: awsFactory,
+		ec2Factory: ec2Factory,
+	}
+}
+
+func (a *GetAMISizeActivity) Execute(ctx context.Context, input GetAMISizeActivityInput) (*GetAMISizeActivityOutput, error) {
+	awsClient, err := a.awsFactory.New(input.OrganizationID, input.SecretID, input.Region)
+	if err != nil {
+		return nil, err
+	}
+
+	ec2Client := a.ec2Factory.New(awsClient)
+
+	describeImagesInput := ec2.DescribeImagesInput{
+		ImageIds: []*string{
+			aws.String(input.ImageID),
+		},
+	}
+	result, err := ec2Client.DescribeImages(&describeImagesInput)
+	if err != nil {
+		return nil, errors.WrapIf(err, "describing AMI failed")
+	} else if len(result.Images) == 0 {
+		return nil, errors.NewWithDetails("describing AMI found no record", "image", input.ImageID)
+	} else if len(result.Images[0].BlockDeviceMappings) == 0 {
+		return nil, errors.NewWithDetails("describing AMI found no block device mappings", "image", input.ImageID)
+	}
+
+	return &GetAMISizeActivityOutput{
+		AMISize: int(aws.Int64Value(result.Images[0].BlockDeviceMappings[0].Ebs.VolumeSize)),
+	}, nil
+}
---
+++ internal/cluster/distribution/eks/eksprovider/workflow/select_volume_size_activity.go
@@ -0,0 +1,70 @@
+// Copyright © 2020 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package workflow
+
+import (
+	"context"
+	"fmt"
+	"math"
+
+	"emperror.dev/errors"
+)
+
+const (
+	// SelectVolumeSizeActivityName is the unique name of the activity.
+	SelectVolumeSizeActivityName = "eks-select-volume-size-activity"
+
+	// unsetVolumeSize is the substituted value for 0/unspecified default volume
+	// size.
+	// nolint:gochecknoglobals // Note: this is a one time configuration, we decided to have in the context of this struct and it is more visible up here than in the middle of the corresponding function.
+	unsetVolumeSize = 50
+)
+
+type SelectVolumeSizeActivity struct {
+	defaultVolumeSize int
+}
+
+type SelectVolumeSizeActivityInput struct {
+	EKSActivityInput
+	AMISize int
+}
+
+type SelectVolumeSizeActivityOutput struct {
+	VolumeSize int
+}
+
+func NewSelectVolumeSizeActivity(defaultVolumeSize int) (activity *SelectVolumeSizeActivity) {
+	return &SelectVolumeSizeActivity{
+		defaultVolumeSize: defaultVolumeSize,
+	}
+}
+
+func (activity *SelectVolumeSizeActivity) Execute(ctx context.Context, input SelectVolumeSizeActivityInput) (output *SelectVolumeSizeActivityOutput, err error) {
+	output = &SelectVolumeSizeActivityOutput{}
+	if activity.defaultVolumeSize > 0 {
+		output.VolumeSize = activity.defaultVolumeSize
+	} else {
+		output.VolumeSize = int(math.Max(float64(unsetVolumeSize), float64(input.AMISize)))
+	}
+
+	if output.VolumeSize < input.AMISize {
+		return nil, errors.New(fmt.Sprintf(
+			"selected volume size of %d GB (default configuration) is less than the AMI size of %d GB",
+			output.VolumeSize, input.AMISize,
+		))
+	}
+
+	return output, nil
+}
