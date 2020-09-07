3af130cc85c7d21bccb77936489362c5e2a9f2d7
colin <kispiszkerf@gmail.com>
2020-09-07 10:21:35 +0200

    eks: validate cluster role ID

--- internal/cluster/distribution/eks/eksprovider/workflow/create_infra.go
+++ internal/cluster/distribution/eks/eksprovider/workflow/create_infra.go
@@ -110,6 +110,25 @@ func CreateInfrastructureWorkflow(ctx workflow.Context, input CreateInfrastructu
 
 	ctx = workflow.WithActivityOptions(ctx, ao)
 
+	// create IAM role validate activity
+	var validateIAMRoleActivityFuture workflow.Future
+	if input.ClusterRoleID != "" {
+		{
+			activityInput := &ValidateIAMRoleActivityInput{
+				EKSActivityInput: commonActivityInput,
+				ClusterRoleID:    input.ClusterRoleID,
+			}
+			validateIAMRoleActivityFuture = workflow.ExecuteActivity(ctx, ValidateIAMRoleActivityName, activityInput)
+		}
+	}
+
+	if validateIAMRoleActivityFuture != nil {
+		validateIAMRoleActivityOutput := ValidateIAMRoleActivityOutput{}
+		if err := validateIAMRoleActivityFuture.Get(ctx, &validateIAMRoleActivityOutput); err != nil {
+			return nil, err
+		}
+	}
+
 	// create IAM roles activity
 	var iamRolesCreateActivityFuture workflow.Future
 	{
---
+++ internal/cluster/distribution/eks/eksprovider/workflow/validate_iam_role.go
@@ -0,0 +1,63 @@
+// Copyright © 2020 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package workflow
+
+import (
+	"context"
+
+	"emperror.dev/errors"
+	"github.com/aws/aws-sdk-go/service/iam"
+)
+
+const ValidateIAMRoleActivityName = "eks-validate-iam-role"
+
+//  ValidateIAMRoleActivity responsible for validating IAM role
+type ValidateIAMRoleActivity struct {
+	awsSessionFactory *AWSSessionFactory
+}
+
+//  ValidateIAMRoleActivityInput holds data needed to validate IAM Role
+type ValidateIAMRoleActivityInput struct {
+	EKSActivityInput
+
+	ClusterRoleID string
+}
+
+//  ValidateIAMRoleActivityOutput holds the output data of ValidateIAMRoleActivity
+type ValidateIAMRoleActivityOutput struct {
+}
+
+//  NewValidateIAMRoleActivity instantiates a new  ValidateIAMRoleActivity
+func NewValidateIAMRoleActivity(awsSessionFactory *AWSSessionFactory) *ValidateIAMRoleActivity {
+	return &ValidateIAMRoleActivity{
+		awsSessionFactory: awsSessionFactory,
+	}
+}
+
+func (a *ValidateIAMRoleActivity) Execute(ctx context.Context, input ValidateIAMRoleActivityInput) (*ValidateIAMRoleActivityOutput, error) {
+	awsSession, err := a.awsSessionFactory.New(input.OrganizationID, input.SecretID, input.Region)
+	if err = errors.WrapIf(err, "failed to create AWS session"); err != nil {
+		return nil, err
+	}
+
+	iamSession := iam.New(awsSession)
+	if _, err := iamSession.GetRole(&iam.GetRoleInput{
+		RoleName: &input.ClusterRoleID,
+	}); err != nil {
+		return nil, errors.WrapIfWithDetails(err, "invalid cluster role ID", "roleID", input.ClusterRoleID)
+	}
+
+	return &ValidateIAMRoleActivityOutput{}, nil
+}
