8b5870e8a88e54d3c79252547c0b5b077251b77b
Patrik Egyed <patrik.egyed@banzaicloud.com>
2020-09-08 22:00:53 +0200

    chore: generated client

--- .gen/pipeline/pipeline/model_eks_update_node_pool_request_all_of.go
+++ .gen/pipeline/pipeline/model_eks_update_node_pool_request_all_of.go
@@ -14,6 +14,9 @@ type EksUpdateNodePoolRequestAllOf struct {
 
 	Autoscaling NodePoolAutoScaling `json:"autoscaling,omitempty"`
 
+	// Size of the EBS volume in GBs of the nodes in the pool.
+	VolumeSize int32 `json:"volumeSize,omitempty"`
+
 	// The instance type to use for your node pool.
 	InstanceType string `json:"instanceType,omitempty"`
 
--- .gen/pipeline/pipeline/model_update_node_pool_request.go
+++ .gen/pipeline/pipeline/model_update_node_pool_request.go
@@ -20,6 +20,9 @@ type UpdateNodePoolRequest struct {
 
 	Autoscaling NodePoolAutoScaling `json:"autoscaling,omitempty"`
 
+	// Size of the EBS volume in GBs of the nodes in the pool.
+	VolumeSize int32 `json:"volumeSize,omitempty"`
+
 	// The instance type to use for your node pool.
 	InstanceType string `json:"instanceType,omitempty"`
 
