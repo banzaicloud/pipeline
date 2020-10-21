690172e72593075aa56deadae8bbacafe9685530
Janos <janos.sarusikis@banzaicloud.com>
2020-10-21 10:50:31 +0200

    Remove code

--- internal/cluster/distribution/pke/node_pool.go
+++ internal/cluster/distribution/pke/node_pool.go
@@ -16,65 +16,7 @@ package pke
 
 import (
 	"context"
-	"crypto/sha1"
-	"fmt"
-
-	"github.com/banzaicloud/pipeline/internal/cluster"
 )
-
-// NewNodePool describes new a Kubernetes node pool in an Amazon cluster.
-type NewNodePool struct {
-	Name        string            `mapstructure:"name"`
-	Labels      map[string]string `mapstructure:"labels"`
-	Size        int               `mapstructure:"size"`
-	Autoscaling struct {
-		Enabled bool `mapstructure:"enabled"`
-		MinSize int  `mapstructure:"minSize"`
-		MaxSize int  `mapstructure:"maxSize"`
-	} `mapstructure:"autoscaling"`
-	VolumeSize   int    `mapstructure:"volumeSize"`
-	InstanceType string `mapstructure:"instanceType"`
-	Image        string `mapstructure:"image"`
-	SpotPrice    string `mapstructure:"spotPrice"`
-	SubnetID     string `mapstructure:"subnetId"`
-}
-
-// Validate semantically validates the new node pool.
-//
-// Some cluster specific compatibility information (eg. subnet settings) should be validated by an external validator.
-func (n NewNodePool) Validate() error {
-	var violations []string
-
-	if n.Autoscaling.Enabled {
-		if n.Autoscaling.MinSize < 0 {
-			violations = append(violations, "minimum autoscaling size cannot be lower than zero")
-		}
-
-		if n.Autoscaling.MaxSize <= n.Autoscaling.MinSize {
-			violations = append(violations, "maximum autoscaling size cannot be lower than the minimum")
-		}
-
-		if n.Size < n.Autoscaling.MinSize {
-			violations = append(violations, "node pool size cannot be lower than the autoscaling minimum size")
-		}
-
-		if n.Size > n.Autoscaling.MaxSize {
-			violations = append(violations, "node pool size cannot be higher than the autoscaling maximum size")
-		}
-	} else if n.Size < 1 {
-		violations = append(violations, "size cannot be lower than one")
-	}
-
-	if n.InstanceType == "" {
-		violations = append(violations, "instance type cannot be empty")
-	}
-
-	if len(violations) > 0 {
-		return cluster.NewValidationError("invalid node pool creation request", violations)
-	}
-
-	return nil
-}
 
 type ExistingNodePool struct {
 	Name          string
@@ -100,14 +42,4 @@ type NodePoolStore interface {
 		clusterID uint,
 		clusterName string,
 	) (existingNodePools map[string]ExistingNodePool, err error)
-}
-
-func CalculateNodePoolVersion(input ...string) string {
-	h := sha1.New() // #nosec
-
-	for _, i := range input {
-		_, _ = h.Write([]byte(i))
-	}
-
-	return fmt.Sprintf("%x", h.Sum(nil))
 }
