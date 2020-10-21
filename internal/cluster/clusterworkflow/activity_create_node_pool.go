2f7dcba735428c0ee958106fbbca99c2815a07c8
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2020-10-21 14:26:22 +0200

    refactor: use new eks node pool creator
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- internal/cluster/clusterworkflow/activity_create_node_pool.go
+++ internal/cluster/clusterworkflow/activity_create_node_pool.go
@@ -16,51 +16,41 @@ package clusterworkflow
 
 import (
 	"context"
-	"fmt"
 
 	"emperror.dev/errors"
-	"github.com/jinzhu/gorm"
-	"github.com/mitchellh/mapstructure"
-	"go.uber.org/cadence/activity"
 
 	"github.com/banzaicloud/pipeline/internal/cluster"
-	"github.com/banzaicloud/pipeline/internal/cluster/distribution/eks"
-	"github.com/banzaicloud/pipeline/internal/cluster/distribution/eks/eksmodel"
-	eksworkflow "github.com/banzaicloud/pipeline/internal/cluster/distribution/eks/eksprovider/workflow"
-	"github.com/banzaicloud/pipeline/internal/global"
 	"github.com/banzaicloud/pipeline/pkg/cadence"
 	"github.com/banzaicloud/pipeline/pkg/providers"
-	sdkAmazon "github.com/banzaicloud/pipeline/pkg/sdk/providers/amazon"
-	"github.com/banzaicloud/pipeline/src/model"
 )
 
 const CreateNodePoolActivityName = "create-node-pool"
 
 type CreateNodePoolActivity struct {
-	clusters          cluster.Store
-	db                *gorm.DB
-	defaultVolumeSize int
-	nodePools         cluster.NodePoolStore
-	eksNodePools      eks.NodePoolStore
-	awsSessionFactory AWSSessionFactory
+	clusters           cluster.Store
+	eksNodePoolCreator NodePoolCreator
+}
+
+// NodePoolCreator creates a new node pool.
+// This is a temporary interface to decouple some EKS specific logic from the clusterworkflow package.
+// Once we get rid of the common node pool create workflow, this will go away as well.
+type NodePoolCreator interface {
+	CreateNodePool(
+		ctx context.Context,
+		userID uint,
+		c cluster.Cluster,
+		rawNodePool cluster.NewRawNodePool,
+	) error
 }
 
 // NewCreateNodePoolActivity returns a new CreateNodePoolActivity.
 func NewCreateNodePoolActivity(
 	clusters cluster.Store,
-	db *gorm.DB,
-	defaultVolumeSize int,
-	nodePools cluster.NodePoolStore,
-	eksNodePools eks.NodePoolStore,
-	awsSessionFactory AWSSessionFactory,
+	eksNodePoolCreator NodePoolCreator,
 ) CreateNodePoolActivity {
 	return CreateNodePoolActivity{
-		clusters:          clusters,
-		db:                db,
-		defaultVolumeSize: defaultVolumeSize,
-		nodePools:         nodePools,
-		eksNodePools:      eksNodePools,
-		awsSessionFactory: awsSessionFactory,
+		clusters:           clusters,
+		eksNodePoolCreator: eksNodePoolCreator,
 	}
 }
 
@@ -71,8 +61,6 @@ type CreateNodePoolActivityInput struct {
 }
 
 func (a CreateNodePoolActivity) Execute(ctx context.Context, input CreateNodePoolActivityInput) error {
-	activityInformation := activity.GetInfo(ctx)
-
 	c, err := a.clusters.GetCluster(ctx, input.ClusterID)
 	if err != nil {
 		return cadence.WrapClientError(err)
@@ -80,195 +68,7 @@ func (a CreateNodePoolActivity) Execute(ctx context.Context, input CreateNodePoo
 
 	switch {
 	case c.Cloud == providers.Amazon && c.Distribution == "eks":
-		var nodePool eks.NewNodePool
-
-		err := mapstructure.Decode(input.RawNodePool, &nodePool)
-		if err != nil {
-			return errors.Wrap(err, "failed to decode node pool")
-		}
-
-		var commonCluster model.ClusterModel
-
-		err = a.db.
-			Where(model.ClusterModel{ID: c.ID}).
-			First(&commonCluster).Error
-		if gorm.IsRecordNotFoundError(err) {
-			return cadence.NewClientError(errors.NewWithDetails(
-				"cluster model is inconsistent",
-				"clusterId", c.ID,
-			))
-		}
-		if err != nil {
-			return errors.WrapWithDetails(
-				err, "failed to get cluster info",
-				"clusterId", c.ID,
-				"nodePoolName", nodePool.Name,
-			)
-		}
-
-		var eksCluster eksmodel.EKSClusterModel
-
-		err = a.db.
-			Where(eksmodel.EKSClusterModel{ClusterID: c.ID}).
-			Preload("Subnets").
-			Preload("Cluster").
-			First(&eksCluster).Error
-		if gorm.IsRecordNotFoundError(err) {
-			return cadence.NewClientError(errors.NewWithDetails(
-				"cluster model is inconsistent",
-				"clusterId", c.ID,
-			))
-		}
-		if err != nil {
-			return errors.WrapWithDetails(
-				err, "failed to get cluster info",
-				"clusterId", c.ID,
-				"nodePoolName", nodePool.Name,
-			)
-		}
-
-		minSize := nodePool.Size
-		maxSize := nodePool.Size + 1
-
-		if nodePool.Autoscaling.Enabled {
-			minSize = nodePool.Autoscaling.MinSize
-			maxSize = nodePool.Autoscaling.MaxSize
-		}
-
-		commonActivityInput := eksworkflow.EKSActivityInput{
-			OrganizationID:            c.OrganizationID,
-			SecretID:                  c.SecretID.ResourceID, // TODO: the underlying secret store is the legacy one
-			Region:                    c.Location,
-			ClusterName:               c.Name,
-			AWSClientRequestTokenBase: sdkAmazon.NewNormalizedClientRequestToken(activity.GetInfo(ctx).WorkflowExecution.ID),
-		}
-
-		if activityInformation.Attempt == 0 {
-			err = a.eksNodePools.CreateNodePool(ctx, eksCluster.ID, input.UserID, nodePool)
-			if err != nil {
-				return err
-			}
-		}
-
-		var vpcActivityOutput *eksworkflow.GetVpcConfigActivityOutput
-		{
-			activityInput := eksworkflow.GetVpcConfigActivityInput{
-				EKSActivityInput: commonActivityInput,
-				StackName:        eksworkflow.GenerateStackNameForCluster(c.Name),
-			}
-
-			var err error
-
-			vpcActivityOutput, err = eksworkflow.NewGetVpcConfigActivity(a.awsSessionFactory).Execute(ctx, activityInput)
-			if err != nil {
-				return err
-			}
-		}
-
-		var amiSize int
-		{
-			activityOutput, err := eksworkflow.NewGetAMISizeActivity(
-				a.awsSessionFactory,
-				eksworkflow.NewEC2Factory(),
-			).Execute(
-				ctx,
-				eksworkflow.GetAMISizeActivityInput{
-					EKSActivityInput: commonActivityInput,
-					ImageID:          nodePool.Image,
-				},
-			)
-			if err != nil {
-				_ = a.eksNodePools.UpdateNodePoolStatus(
-					ctx,
-					c.OrganizationID,
-					c.ID,
-					c.Name,
-					nodePool.Name,
-					eks.NodePoolStatusError,
-					fmt.Sprintf("Validation failed: retrieving AMI size failed: %s", err),
-				)
-
-				return err
-			}
-
-			amiSize = activityOutput.AMISize
-		}
-
-		var volumeSize int
-		{
-			activityOutput, err := eksworkflow.NewSelectVolumeSizeActivity(
-				a.defaultVolumeSize,
-			).Execute(
-				ctx,
-				eksworkflow.SelectVolumeSizeActivityInput{
-					AMISize:            amiSize,
-					OptionalVolumeSize: nodePool.VolumeSize,
-				},
-			)
-			if err != nil {
-				_ = a.eksNodePools.UpdateNodePoolStatus(
-					ctx,
-					c.OrganizationID,
-					c.ID,
-					c.Name,
-					nodePool.Name,
-					eks.NodePoolStatusError,
-					fmt.Sprintf("Validation failed: selecting volume size failed: %s", err),
-				)
-
-				return err
-			}
-
-			volumeSize = activityOutput.VolumeSize
-		}
-
-		subinput := eksworkflow.CreateAsgActivityInput{
-			EKSActivityInput: commonActivityInput,
-			ClusterID:        input.ClusterID,
-			StackName:        eksworkflow.GenerateNodePoolStackName(c.Name, nodePool.Name),
-
-			ScaleEnabled: commonCluster.ScaleOptions.Enabled,
-
-			Subnets: []eksworkflow.Subnet{
-				{
-					SubnetID: nodePool.SubnetID,
-				},
-			},
-
-			VpcID:               vpcActivityOutput.VpcID,
-			SecurityGroupID:     vpcActivityOutput.SecurityGroupID,
-			NodeSecurityGroupID: vpcActivityOutput.NodeSecurityGroupID,
-			NodeInstanceRoleID:  eksCluster.NodeInstanceRoleId,
-
-			Name:             nodePool.Name,
-			NodeSpotPrice:    nodePool.SpotPrice,
-			Autoscaling:      nodePool.Autoscaling.Enabled,
-			NodeMinCount:     minSize,
-			NodeMaxCount:     maxSize,
-			Count:            nodePool.Size,
-			NodeVolumeSize:   volumeSize,
-			NodeImage:        nodePool.Image,
-			NodeInstanceType: nodePool.InstanceType,
-			Labels:           nodePool.Labels,
-			Tags:             eksCluster.Cluster.Tags,
-		}
-
-		eksConfig := global.Config.Distribution.EKS
-		if eksConfig.SSH.Generate {
-			subinput.SSHKeyName = eksworkflow.GenerateSSHKeyNameForCluster(c.Name)
-		}
-
-		nodePoolTemplate, err := eksworkflow.GetNodePoolTemplate()
-		if err != nil {
-			return errors.WrapIf(err, "failed to get CloudFormation template for node pools")
-		}
-
-		_, err = eksworkflow.NewCreateAsgActivity(
-			a.awsSessionFactory, nodePoolTemplate, a.eksNodePools,
-		).Execute(ctx, subinput)
-		if err != nil {
-			return cadence.WrapClientError(err)
-		}
+		return a.eksNodePoolCreator.CreateNodePool(ctx, input.UserID, c, input.RawNodePool)
 	default:
 		return cadence.WrapClientError(errors.WithStack(cluster.NotSupportedDistributionError{
 			ID:           c.ID,
@@ -278,6 +78,4 @@ func (a CreateNodePoolActivity) Execute(ctx context.Context, input CreateNodePoo
 			Message: "the node pool API does not support this distribution yet",
 		}))
 	}
-
-	return nil
 }
