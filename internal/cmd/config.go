53215e0a00931027412ab0741731fe30ccb40bbc
Magyari Sandor Szilard <sancyx@gmail.com>
2020-10-28 19:51:05 +0100

    use latest helm chart

--- internal/cmd/config.go
+++ internal/cmd/config.go
@@ -769,7 +769,7 @@ traefik:
 	v.SetDefault("cluster::disasterRecovery::ark::backupSyncInterval", "20s")
 	v.SetDefault("cluster::disasterRecovery::ark::restoreWaitTimeout", "5m")
 	v.SetDefault("cluster::disasterRecovery::charts::ark::chart", "banzaicloud-stable/velero")
-	v.SetDefault("cluster::disasterRecovery::charts::ark::version", "2.13.3-bnz1")
+	v.SetDefault("cluster::disasterRecovery::charts::ark::version", "2.13.3-bc.1")
 	v.SetDefault("cluster::disasterRecovery::charts::ark::values", map[string]interface{}{
 		"image": map[string]interface{}{
 			"repository": "velero/velero",
