f660f96db6ca68b76ae45404fe383974f6104022
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2020-11-05 22:02:11 +0100

    feat(cadence): add string context propagator
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ pkg/cadence/context.go
@@ -0,0 +1,45 @@
+// Copyright © 2020 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cadence
+
+import (
+	"time"
+
+	"go.uber.org/cadence/workflow"
+)
+
+// An emptyCtx is never canceled, has no values, and has no deadline.  It is not
+// struct{}, since vars of this type must have distinct addresses.
+type emptyCtx int
+
+func (*emptyCtx) Deadline() (deadline time.Time, ok bool) {
+	return
+}
+
+func (*emptyCtx) Done() workflow.Channel {
+	return nil
+}
+
+func (*emptyCtx) Err() error {
+	return nil
+}
+
+func (*emptyCtx) Value(key interface{}) interface{} {
+	return nil
+}
+
+func (e *emptyCtx) String() string {
+	return "empty Context"
+}
