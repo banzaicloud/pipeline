b953bd29e0ef9f5912f1e64d965b0870c004454f
Laszlo Puskas <laszlo@banzaicloud.com>
2020-11-23 22:14:43 +0100

    Simplify the reconciler interface -and the logic it implements

--- internal/integratedservices/services/dns/reconciler.go
+++ internal/integratedservices/services/dns/reconciler.go
@@ -16,7 +16,6 @@ package dns
 
 import (
 	"context"
-	"reflect"
 	"sort"
 
 	"emperror.dev/errors"
@@ -25,50 +24,40 @@ import (
 	"github.com/banzaicloud/operator-tools/pkg/utils"
 	"golang.org/x/mod/semver"
 	errors2 "k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	"github.com/banzaicloud/pipeline/internal/integratedservices"
+	"github.com/banzaicloud/pipeline/internal/common"
 )
 
 // Reconciler decouples handling of custom resources on a kubernetes cluster
 type Reconciler interface {
 	// Reconcile creates and applies CRs to a cluster
-	Reconcile(ctx context.Context, kubeConfig []byte, config Config, values []byte, spec integratedservices.IntegratedServiceSpec, delete bool) error
+	Reconcile(ctx context.Context, kubeConfig []byte, svcInstance v1alpha1.ServiceInstance) error
 }
 
 // isvcReconciler components struct in charge for assembling the CR manifest  and applying it to a cluster (by delegating to a cluster client)
 type isvcReconciler struct {
 	scheme *runtime.Scheme
-	// todo infer a logger (for debugging purposes)
+	logger common.Logger
 }
 
 // NewISReconciler builds an integrated service reconciler
-func NewISReconciler() Reconciler {
+func NewISReconciler(logger common.Logger) Reconciler {
 	// register needed shemes
 	scheme := runtime.NewScheme()
 	_ = clientgoscheme.AddToScheme(scheme)
 	_ = v1alpha1.AddToScheme(scheme)
 	return isvcReconciler{
 		scheme: scheme,
+		logger: logger,
 	}
 }
 
-func (is isvcReconciler) Reconcile(ctx context.Context, kubeConfig []byte, config Config, values []byte, spec integratedservices.IntegratedServiceSpec, delete bool) error {
-	si := &v1alpha1.ServiceInstance{
-		ObjectMeta: metav1.ObjectMeta{
-			Namespace: "default",      // todo get this from the  spec (or input arg)
-			Name:      "external-dns", // todo  get this from spec (or input arg)
-		},
-		Spec: v1alpha1.ServiceInstanceSpec{
-			Service: "external-dns", // TODO get the name from the spec (possibly transform it)
-			Config:  string(values),
-		},
-	}
-
+func (is isvcReconciler) Reconcile(ctx context.Context, kubeConfig []byte, incomingSI v1alpha1.ServiceInstance) error {
+	is.logger.Debug("reconciling integrated service instance ...")
 	restCfg, err := clientcmd.RESTConfigFromKubeConfig(kubeConfig)
 	if err != nil {
 		return errors.Wrap(err, "failed to create rest config from cluster configuration")
@@ -79,60 +68,54 @@ func (is isvcReconciler) Reconcile(ctx context.Context, kubeConfig []byte, confi
 		return errors.Wrap(err, "failed to create the client from rest configuration")
 	}
 
-	key, okErr := client.ObjectKeyFromObject(si)
+	key, okErr := client.ObjectKeyFromObject(&incomingSI)
 	if okErr != nil {
 		return errors.Wrap(err, "failed to get object key for lookup")
 	}
 
-	lookupSI := &v1alpha1.ServiceInstance{}
-	if err := cli.Get(ctx, key, lookupSI); err != nil {
-		if !errors2.IsNotFound(err) {
+	existingSI := v1alpha1.ServiceInstance{}
+	isNew := false
+	if err := cli.Get(ctx, key, &existingSI); err != nil {
+		if errors2.IsNotFound(err) {
+			isNew = true
+		} else {
 			return errors.Wrap(err, "failed to look up service instance")
 		}
 	}
 
+	// TODO is t he  existence check superflueous here? simply call the method below - apparently it returns all the information needed (we could spare the above lookup)
 	resourceReconciler := reconciler.NewReconcilerWith(cli)
-	emptySI := v1alpha1.ServiceInstance{} // for better  readability
-	if reflect.DeepEqual(lookupSI, &emptySI) {
-		if delete {
-			// do nothing as the instance doesn't exist
-			return nil
-		}
-
-		_, _, err := resourceReconciler.CreateIfNotExist(si, reconciler.StateCreated)
+	if isNew {
+		_, object, err := resourceReconciler.CreateIfNotExist(&incomingSI, reconciler.StateCreated)
 		if err != nil {
 			return errors.Wrap(err, "failed to create the service instance resource")
 		}
 
-		// retrieve the newly created CR
-		// TODO is it possible to "cast" the result of the above call?
-		if err := cli.Get(ctx, key, lookupSI); err != nil {
-			return errors.Wrap(err, "failed to look up service instance")
+		if newSvcInstance, ok := object.(*v1alpha1.ServiceInstance); ok {
+			existingSI = *newSvcInstance
+		} else {
+			return errors.Wrap(err, "failed to create the service instance resource")
 		}
 	}
 
-	// at this point we have the CR created and retrieved
-	if lookupSI.Spec.Enabled == nil {
-		si.Spec.Enabled = utils.BoolPointer(true)
-		if si.Spec.Version == "" {
-			// If version is not provided proceed with the latest available version
-			latestVersion, err := is.getLatestVersion(*lookupSI)
-			if err != nil {
-				return errors.Wrap(err, "failed to get the  latest version")
-			}
-			si.Spec.Version = latestVersion
-		}
+	// at this point the incoming changes need to be applied to the existing instance - that'll be updated
+	existingSI.Spec.Enabled = incomingSI.Spec.Enabled
+	// make sure the flag is populated / enable it by default
+	if existingSI.Spec.Enabled == nil {
+		existingSI.Spec.Enabled = utils.BoolPointer(true)
 	}
 
-	if delete {
-		if _, err := resourceReconciler.ReconcileResource(si, reconciler.StateAbsent); err != nil {
-			return errors.Wrap(err, "failed to reconcile the integrated service")
+	existingSI.Spec.Version = incomingSI.Spec.Version
+	// make sure the version is populated / set the latest available version by default
+	if incomingSI.Spec.Version == "" {
+		latestVersion, err := is.getLatestVersion(existingSI)
+		if err != nil {
+			return errors.Wrap(err, "failed to get the  latest version")
 		}
-		return nil
+		existingSI.Spec.Version = latestVersion
 	}
 
-	// TODO this is the update case- is the spec OK?
-	if _, err := resourceReconciler.ReconcileResource(si, reconciler.StatePresent); err != nil {
+	if _, err := resourceReconciler.ReconcileResource(&existingSI, reconciler.StatePresent); err != nil {
 		return errors.Wrap(err, "failed to reconcile the integrated service")
 	}
 
