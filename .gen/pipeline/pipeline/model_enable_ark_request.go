3ce110aa0c3da6a21b6cf26bbf49ac7765e4f937
Magyari Sandor Szilard <sancyx@gmail.com>
2020-12-08 14:23:57 +0100

    add serviceAccountRoleARN field to enable backup call

--- .gen/pipeline/pipeline/model_enable_ark_request.go
+++ .gen/pipeline/pipeline/model_enable_ark_request.go
@@ -27,6 +27,9 @@ type EnableArkRequest struct {
 	// relevant only in case of Amazon clusters. By default set to false in which case you must add snapshot permissions to your node instance role. Should you set to true Pipeline will deploy your cluster secret to the cluster.
 	UseClusterSecret bool `json:"useClusterSecret,omitempty"`
 
+	// relevant only in case of Amazon clusters. This a third option to give permissions for volume snapshots to Velero, besides the default NodeInstance role or cluster secret deployment.
+	ServiceAccountRoleARN string `json:"serviceAccountRoleARN,omitempty"`
+
 	// required only case of Azure
 	StorageAccount string `json:"storageAccount,omitempty"`
 
