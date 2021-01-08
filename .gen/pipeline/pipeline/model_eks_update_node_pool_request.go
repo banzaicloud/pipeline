7b4af45bdfa9c9357a213b32e66ed413927cd1a7
Magyari Sandor Szilard <sancyx@gmail.com>
2021-01-08 19:37:36 +0100

    regenrate API

--- .gen/pipeline/pipeline/model_eks_update_node_pool_request.go
+++ .gen/pipeline/pipeline/model_eks_update_node_pool_request.go
@@ -42,7 +42,7 @@ type EksUpdateNodePoolRequest struct {
 	SecurityGroups *[]string `json:"securityGroups,omitempty"`
 
 	// Setup available instance stores (NVMe disks) to use for Kubelet root if available. As a result emptyDir volumes will be provisioned on local instance storage disks. You can check out available instance storages here https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/InstanceStorage.html#instance-store-volumes.
-	UseInstanceStore bool `json:"useInstanceStore,omitempty"`
+	UseInstanceStore *bool `json:"useInstanceStore,omitempty"`
 
 	Options BaseUpdateNodePoolOptions `json:"options,omitempty"`
 }
