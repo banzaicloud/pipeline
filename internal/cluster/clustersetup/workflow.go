c53869c681912a23c3d307ddb3f68a55dc800084
Laszlo Puskas <laszlo@banzaicloud.com>
2021-01-23 14:20:39 +0100

    feat(is-op): Add the install IS operator activity to the  clustersetup workflow

--- internal/cluster/clustersetup/workflow.go
+++ internal/cluster/clustersetup/workflow.go
@@ -17,8 +17,11 @@ package clustersetup
 import (
 	"time"
 
+	"emperror.dev/errors"
 	"go.uber.org/cadence"
 	"go.uber.org/cadence/workflow"
+
+	"github.com/banzaicloud/pipeline/internal/cluster/workflow/isoperator"
 )
 
 // WorkflowName can be used to reference the cluster setup workflow.
@@ -128,6 +131,14 @@ func (w Workflow) Execute(ctx workflow.Context, input WorkflowInput) error {
 		err := workflow.ExecuteActivity(ctx, ConfigureNodePoolLabelsActivityName, activityInput).Get(ctx, nil)
 		if err != nil {
 			return err
+		}
+	}
+
+	{
+		// install / upgrade the  integrated service operator
+		input := isoperator.NewISOperatorInstallerActivityInput(input.Organization.ID, input.Cluster.ID)
+		if err := workflow.ExecuteActivity(ctx, isoperator.ISOperatorInstallerActivityName, input).Get(ctx, nil); err != nil {
+			return errors.WrapIfWithDetails(err, "failed to install the  operator", "orgID", input.OrgID, "clusterID", input.ClusterID)
 		}
 	}
 
