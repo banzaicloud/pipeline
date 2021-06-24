0e444dbcda2e813a190bc164525c653392683914
Patrik Egyed <pregnor@cisco.com>
2021-06-24 10:19:48 +0200

    chore(gen): regenerated Pipeline OAS files

--- .gen/pipeline/pipeline/model_eks_node_pool_all_of.go
+++ .gen/pipeline/pipeline/model_eks_node_pool_all_of.go
@@ -19,6 +19,9 @@ type EksNodePoolAllOf struct {
 	// Size of the EBS volume in GBs of the nodes in the pool.
 	VolumeSize int32 `json:"volumeSize,omitempty"`
 
+	// Type of the EBS volume of the nodes in the pool (default gp2).
+	VolumeType string `json:"volumeType,omitempty"`
+
 	// Machine instance type.
 	InstanceType string `json:"instanceType"`
 
